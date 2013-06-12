94c03e7717d1ed6985856ab87a7a7acddfd79717
Chad Horohoe <chadh@wikimedia.org>
2013-06-12 19:59:33 +0000

    Initial version number

--- CirrusSearch.php
+++ CirrusSearch.php
@@ -14,6 +14,7 @@ $wgExtensionCredits['other'][] = array(
 	'author'         => array( 'Nik Everett', 'Chad Horohoe' ),
 	'descriptionmsg' => 'cirrussearch-desc',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:MWSearch',
+	'version'        => '0.1'
 );
 
 /**

613536f8728bbc3d0abf68a61f3cb7625e0dd6b2
Nik Everett <neverett@wikimedia.org>
2013-07-11 14:47:36 +0000

    Move elasticsearch work onto its own branch.
    Change-Id: I201996e0e0184af4e9757523a4da6f2d64915f49

--- CirrusSearch.body.php
+++ CirrusSearch.body.php
@@ -21,71 +21,68 @@ class CirrusSearch extends SearchEngine {
 	/**
 	 * Singleton instance of the client
 	 *
-	 * @var Solarium_Client
+	 * @var \Elastica\Index
 	 */
-	private static $client = null;
+	private static $index = null;
 
 	/**
-	 * Fetch the Solr client.
+	 * Fetch the Elastica Index.
 	 *
-	 * @return Solarium_Client
+	 * @return \Elastica\Index
 	 */
-	static function getClient() {
-		if ( self::$client != null ) {
-			return self::$client;
+	public static function getIndex() {
+		if ( self::$index != null ) {
+			return self::$index;
 		}
 		global $wgCirrusSearchServers, $wgCirrusSearchMaxRetries;
 
-		self::$client = new Solarium_Client();
-		self::$client->setAdapter( 'Solarium_Client_Adapter_Curl' );
-	
-		// Setup the load balancer
-		$loadBalancer = self::$client->getPlugin( 'loadbalancer' );
-
-		// Allow updates to be load balancer just like creates
-		$loadBalancer->clearBlockedQueryTypes();
-
-		// Setup failover
-		if ( $wgCirrusSearchMaxRetries > 1 ) { 
-			$loadBalancer->setFailoverEnabled( true );
-			$loadBalancer->setFailoverMaxRetries( $wgCirrusSearchMaxRetries );
-		}
-
-		// Setup the Solr endpoints
+		// Setup the Elastica endpoints
+		$servers = array();
 		foreach ( $wgCirrusSearchServers as $server ) {
-			$serverConfig = array( 
-				'host' => $server,
-				'core' => wfWikiId()
-			);
-			$loadBalancer->addServer( $server, $serverConfig, 1 );
+			$servers[] = array('host' => $server);
 		}
-		return self::$client;
+		$client = new \Elastica\Client( array(
+			'servers' => $servers
+		) );
+		self::$index = $client->getIndex( wfWikiId() );
+
+		return self::$index;
+	}
+
+	/**
+	 * Fetch the Elastica Type for pages.
+	 *
+	 * @ \Elastica\Type
+	 */
+	static function getPageType() {
+		return CirrusSearch::getIndex()->getType('page');
 	}
 
 	public static function prefixSearch( $ns, $search, $limit, &$results ) {
+		wfDebugLog( 'CirrusSearch', "Prefix searching:  $search" );
 		// Boilerplate
 		$nsNames = RequestContext::getMain()->getLanguage()->getNamespaces();
-		$client = self::getClient();
-		$query = $client->createSelect();
+		$query = new Elastica\Query();
+		$query->setFields( array( 'id', 'title', 'namespace' ) );
 
 		// Query params
-		$query->setRows( $limit );
-		wfDebugLog( 'CirrusSearch', "Prefix searching:  $search" );
-		if( count( $ns ) ) {
-			$query = self::setNamespaceFilter( $ns, $query );
-		}
-		$query->setQuery( 'titlePrefix:%T1%', array( $search  ) );
+		$query->setLimit( $limit );
+		$query->setFilter( CirrusSearch::buildNamespaceFilter( $ns ) );
+		// TODO remove the strtolower and let elasticsearch do it because it is a tokenizing beast
+		$query->setQuery( new \Elastica\Query\Prefix( array( 'title' => strtolower( $search ) ) ) );
 
 		// Perform the search
 		try {
-			$res = $client->select( $query );
+			$result = CirrusSearch::getPageType()->search( $query );
+			wfDebugLog( 'CirrusSearch', 'Search completed in ' . $result->getTotalTime() . ' millis' );
 		} catch ( Solarium_Exception $e ) {
+			// TODO find the right exception
 			wfLogWarning( "Search backend error during title prefix search for '$search'." );
 			return false;
 		}
 
 		// We only care about title results
-		foreach( $res as $r ) {
+		foreach( $result->getResults() as $r ) {
 			$results[] = Title::makeTitle( $r->namespace, $r->title )->getPrefixedText();
 		}
 
@@ -93,13 +90,9 @@ class CirrusSearch extends SearchEngine {
 	}
 
 	public function searchText( $term ) {
+		wfDebugLog( 'CirrusSearch', "Searching:  $term" );
+		
 		$originalTerm = $term;
-		function addHighlighting( $highlighting, $term ) {
-			if ( $highlighting->getQuery() !== null ) {
-				$term = $highlighting->getQuery() . ' OR ' . $term;
-			}
-			$highlighting->setQuery( $term );
-		}
 
 		// Ignore leading ~ because it is used to force displaying search results but not to effect them
 		if ( substr( $term, 0, 1 ) === '~' )  {
@@ -107,45 +100,41 @@ class CirrusSearch extends SearchEngine {
 		}
 
 		// Boilerplate
-		$client = self::getClient();
-		$query = $client->createSelect();
+		$query = new Elastica\Query();
 		$query->setFields( array( 'id', 'title', 'namespace' ) );
+
+		$filters = array();
 
 		// Offset/limit
 		if( $this->offset ) {
-			$query->setStart( $this->offset );
+			$query->setFrom( $this->offset );
 		}
 		if( $this->limit ) {
-			$query->setRows( $this->limit );
+			$query->setLimit( $this->limit );
 		}
-
-		// Namespaces
-		$query = self::setNamespaceFilter( $this->namespaces, $query );
-
-		$dismax = $query->getDismax();
-		$dismax->setQueryParser( 'edismax' );
-		$dismax->setPhraseFields( 'title^1000.0 text^1000.0' );
-		$dismax->setPhraseSlop( '3' );
-		$dismax->setQueryFields( 'title^20.0 text^3.0' );
-
-		$highlighting = $query->getHighlighting();
+		$filters[] = CirrusSearch::buildNamespaceFilter( $this->namespaces );
 
 		$term = preg_replace_callback(
 			'/(?<key>[^ ]+):(?<value>(?:"[^"]+")|(?:[^ ]+)) ?/',
-			function ( $matches ) use ( $query, $highlighting ) {
+			function ( $matches ) use ( &$filters ) {
 				$key = $matches['key'];
 				$value = trim( $matches['value'], '"' );
 				switch ( $key ) {
 					case 'incategory':
-						$query->createFilterQuery( "$key:$value" )->setQuery( '+category:%P1%', array( $value ) );
+						$filters[] = new \Elastica\Filter\Term( array( 'category' => $value ) );
+						// TODO I used to add highlighting here for $value
 						return '';
 					case 'prefix':
-						$query->createFilterQuery( "$key:$value" )->setQuery( '+titlePrefix:%P1% OR +textPrefix:%P1%', array( $value ) );
-						addHighlighting( $highlighting, "$value*" );
+						// TODO maybe we're better 
+						$filter = new \Elastica\Filter\BoolOr();
+						$filter->addFilter( new \Elastica\Filter\Prefix( 'title', $value ) );
+						$filter->addFilter( new \Elastica\Filter\Prefix( 'text', $value ) );
+						$filters[] = $filter;
+						// TODO I used to add highlighting here for $value*
 						return '';
 					case 'intitle':
-						$query->createFilterQuery( "$key:$value" )->setQuery( '+title:%P1%', array( $value ) );
-						addHighlighting( $highlighting, $value );
+						$filters[] = new \Elastica\Filter\Term( array( 'title' => $value ) );
+						// TODO I used to add highlighting here for $value
 						return '';
 					default:
 						return $matches[0];
@@ -154,36 +143,42 @@ class CirrusSearch extends SearchEngine {
 			$term
 		);
 
-		if ( $this->namespaces !== null ) {
-			$query->createFilterQuery( 'namspaces' )->setQuery( '+namespace:(' . implode( ' OR ', $this->namespaces ) . ')' );
-		}
+		// TODO do I really not have to/get to specify the terms to try to highlight?
+		// This seems out of the style of the rest of the Elastica....
+		$query->setHighlight( array( 
+			'pre_tags' => array( '<span class="searchmatch">' ),
+			'post_tags' => array( '</span>' ),
+			'fields' => array(
+				'title' => array( 'number_of_fragments' => 0 ), // Don't fragment the title - it is too small.
+				'text' => array( 'number_of_fragments' => 1 )
+			)
+		) );
 
-		/*
-		 * Escape some special characters that we don't want users to pass to solr directly.
-		 * These special characters _aren't_ escaped: * and ~
-		 * *: Do a prefix or postfix search against the stemmed text which isn't strictly a good
-		 * idea but this is so rarely used that adding extra code to flip prefix searches into
-		 * real prefix searches isn't really worth it.  The same goes for postfix searches but
-		 * doubly because we don't have a postfix index (backwards ngram.)
-		 * ~: Do a fuzzy match against the stemmed text which isn't strictly a good idea but it
-		 * gets the job done and fuzzy matches are a really rarely used feature to be creating an
-		 * extra index for.
-		 */
-		$term = preg_replace ( '/(\+|-|&&|\|\||!|\(|\)|\{|}|\[|]|\^|"|\?|:|\\\)/', '\\\$1', $term );
+		$filters = array_filter( $filters );
+		if ( count( $filters ) ) {
+			$filter = new \Elastica\Filter\BoolAnd();
+			$filter->setFilters( $filters );
+			$query->setFilter( $filter );
+			wfDebugLog( 'CirrusSearch', 'Setting up ' . count( $filters ) . ' filter' );
+		}
 
 		// Actual text query
-		if ( trim( $term ) === '' ) {
-			$term = '*:*';
-		} else {
-			$spellCheck = $query->getSpellCheck()->setQuery( $term );
-			addHighlighting( $highlighting, $term );
+		if ( trim( $term ) !== '' ) {
+			// TODO make sure the query is well formed or make elasticsearch do it.
+			$queryStringQuery = new \Elastica\Query\QueryString( $term );
+			$queryStringQuery->setFields( array( 'title^20.0', 'text^3.0' ) );
+			$queryStringQuery->setAutoGeneratePhraseQueries( true );
+			$queryStringQuery->setPhraseSlop( 3 );
+			// TODO phrase match boosts?
+			$query->setQuery( $queryStringQuery );
+			// TODO spellcheck
 		}
-		wfDebugLog( 'CirrusSearch', "Searching:  $term" );
-		$query->setQuery( $term );
 
 		// Perform the search and return a result set
 		try {
-			return new CirrusSearchResultSet( $client->select( $query ) );
+			$result = CirrusSearch::getPageType()->search( $query );
+			wfDebugLog( 'CirrusSearch', 'Search completed in ' . $result->getTotalTime() . ' millis' );
+			return new CirrusSearchResultSet( $result );
 		} catch ( Solarium_Exception $e ) {
 			$status = new Status();
 			$status->warning( 'cirrussearch-backend-error' );
@@ -196,13 +191,12 @@ class CirrusSearch extends SearchEngine {
 	 * Filter a query to only return results in given namespace(s)
 	 *
 	 * @param array $ns Array of namespaces
-	 * @param Solarium_Query $query
-	 * @return Solarium_Query
 	 */
-	private static function setNamespaceFilter( array $ns, $query ) {
-		$query->createFilterQuery( 'namespace' )
-				->setQuery( 'namespace:' . implode( ' OR ', $ns ) );
-		return $query;
+	private static function buildNamespaceFilter( array $ns ) {
+		if ( $ns !== null && count( $ns ) ) {
+			return new \Elastica\Filter\Terms( 'namespace', $ns );
+		}
+		return null;
 	}
 
 	public function update( $id, $title, $text ) {
@@ -247,30 +241,29 @@ class CirrusSearchResultSet extends SearchResultSet {
 
 	public function __construct( $res ) {
 		$this->result = $res;
-		$this->docs = $res->getDocuments();
 		$this->hits = $res->count();
-		$this->totalHits = $res->getNumFound();
-		$spellcheck = $res->getSpellcheck();
+		$this->totalHits = $res->getTotalHits();
+		// $spellcheck = $res->getSpellcheck();
 		$this->suggestionQuery = null;
 		$this->suggestionSnippet = null;
-		if ( $spellcheck !== null && !$spellcheck->getCorrectlySpelled()  ) {
-			$collation = $spellcheck->getCollation();
-			if ( $collation !== null ) {
-				$this->suggestionQuery = $collation->getQuery();
-				$keys = array();
-				$highlightedKeys = array();
-				foreach ( $collation->getCorrections() as $misspelling => $correction ) {
-					// Oddly Solr will sometimes claim that a word is misspelled and then not provide a better spelling for it.
-					if ( $misspelling === $correction ) {
-						continue;
-					}
-					// TODO escaping danger
-					$keys[] = "/$correction/";
-					$highlightedKeys[] = "<em>$correction</em>";
-				}
-				$this->suggestionSnippet = preg_replace( $keys, $highlightedKeys, $this->suggestionQuery );
-			}
-		}
+		// if ( $spellcheck !== null && !$spellcheck->getCorrectlySpelled()  ) {
+		// 	$collation = $spellcheck->getCollation();
+		// 	if ( $collation !== null ) {
+		// 		$this->suggestionQuery = $collation->getQuery();
+		// 		$keys = array();
+		// 		$highlightedKeys = array();
+		// 		foreach ( $collation->getCorrections() as $misspelling => $correction ) {
+		// 			// Oddly Solr will sometimes claim that a word is misspelled and then not provide a better spelling for it.
+		// 			if ( $misspelling === $correction ) {
+		// 				continue;
+		// 			}
+		// 			// TODO escaping danger
+		// 			$keys[] = "/$correction/";
+		// 			$highlightedKeys[] = "<em>$correction</em>";
+		// 		}
+		// 		$this->suggestionSnippet = preg_replace( $keys, $highlightedKeys, $this->suggestionQuery );
+		// 	}
+		// }
 	}
 
 	public function hasResults() {
@@ -298,13 +291,12 @@ class CirrusSearchResultSet extends SearchResultSet {
 	}
 
 	public function next() {
-		static $pos = 0;
-		$solrResult = null;
-		if( isset( $this->docs[$pos] ) ) {
-			$solrResult = new CirrusSearchResult( $this->result, $this->docs[$pos] );
-			$pos++;
+		$current = $this->result->current();
+		if ( $current ) {
+			$this->result->next();
+			return new CirrusSearchResult( $current );	
 		}
-		return $solrResult;
+		return false;
 	}
 }
 
@@ -314,19 +306,17 @@ class CirrusSearchResultSet extends SearchResultSet {
 class CirrusSearchResult extends SearchResult {
 	private $titleSnippet, $textSnippet;
 
-	public function __construct( $result, $doc ) {
-		$fields = $doc->getFields();
-		$highlighting = $result->getHighlighting()->getResult( $fields[ 'id' ] )->getFields();
-
-		$this->initFromTitle( Title::makeTitle( $fields['namespace'], $fields[ 'title' ] ) );
-		if ( isset( $highlighting[ 'title' ] ) ) {
+	public function __construct( $result ) {
+		$this->initFromTitle( Title::makeTitle( $result->namespace, $result->title ) );
+		$highlights = $result->getHighlights();
+		if ( isset( $highlights[ 'title' ] ) ) {
 			// @todo: This should also show the namespace, we know it
-			$this->titleSnippet = $highlighting[ 'title' ][ 0 ];
+			$this->titleSnippet = $highlights[ 'title' ][ 0 ];
 		} else {
 			$this->titleSnippet = '';
 		}
-		if ( isset( $highlighting[ 'text' ] ) ) {
-			$this->textSnippet = $highlighting[ 'text' ][ 0 ];
+		if ( isset( $highlights[ 'text' ] ) ) {
+			$this->textSnippet = $highlights[ 'text' ][ 0 ];
 		} else {
 			list( $contextLines, $contextChars ) = SearchEngine::userHighlightPrefs();
 			$this->initText();
---
+++ updateElasticsearchIndex.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Generate the Solr configuration
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+require_once( "maintenance/Maintenance.php" );
+
+/**
+ * Build a solr config directory.
+ */
+class UpdateElasticsearchIndex extends Maintenance {
+	private $rebuild, $closeOk;
+
+	// Is the index currently closed?
+	private $closed = false;
+	// Is a reindex required
+	private $reindexRequired = false;
+
+	private $reindexChunkSize = 1000;
+
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Update the elasticsearch index for this wiki";
+		$this->addOption( 'rebuild', 'Rebuild the index.' );
+		$this->addOption( 'forceOpen', "Open the index but do nothing else.  Use this if " .
+			"you've stuck the index closed and need it to start working right now." );
+		$this->addOption( 'closeOk', "Allow the script to close the index if decides it has " .
+			"to.  Note that it is never ok to close an index that you just created." );
+		$this->addOption( 'forceReindex', "Perform a reindex right now." );
+	}
+	public function execute() {
+		if ( $this->getOption( 'forceOpen', false) ) {
+			CirrusSearch::getIndex()->open();
+			return;
+		}
+		if ( $this->getOption( 'forceReindex', false) ) {
+			$this->reindex();
+			return;
+		}
+		$this->rebuild = $this->getOption( 'rebuild', false );
+		$this->closeOk = $this->getOption( 'closeOk', false );
+
+		$this->validateIndex();
+		$this->validateAnalyzers();
+		$this->validateMapping();
+
+		if ($this->closed) {
+			CirrusSearch::getIndex()->open();
+		}
+	}
+
+	private function validateIndex() {
+		if ( $this->rebuild ) {
+			$this->output( "Rebuilding index..." );
+			$this->createIndex( true );
+			$this->output( "ok\n" );
+			return;
+		}
+		if ( !CirrusSearch::getIndex()->exists() ) {
+			$this->output( "Creating index..." );
+			$this->createIndex( false );
+			$this->output( "ok\n" );
+			return;
+		}
+		$this->output( "Index exists so validating...\n" );
+		global $wgCirrusSearchShardCount, $wgCirrusSearchReplicaCount;
+		$settings = CirrusSearch::getIndex()->getSettings()->get();
+
+		$this->output( "\tValidating number of shards..." );
+		$actualShardCount = $settings['index.number_of_shards'];
+		if ( $actualShardCount == $wgCirrusSearchShardCount ) {
+			$this->output( "ok\n" );
+		} else {
+			$this->output( "is $actualShardCount but should be $wgCirrusSearchShardCount...cannot correct!\n" );
+			$this->error(
+				"Number of shards is incorrect. You can run this script with --rebuild to\n" .
+				"correct the problem but it will blow away the index and you'll have to reindex\n" .
+				"the contents.  This script will now continue to validate everything else." );
+		}
+
+		$this->output( "\tValidating number of replicas..." );
+		$actualReplicaCount = $settings['index.number_of_replicas'];
+		if ( $actualReplicaCount == $wgCirrusSearchReplicaCount ) {
+			$this->output( "ok\n" );
+		} else {
+			$this->output( "is $actualReplicaCount but should be $wgCirrusSearchReplicaCount..." );
+			CirrusSearch::getIndex()->getSettings()->setNumberOfReplicas( $wgCirrusSearchReplicaCount );
+			$this->output( "corrected\n" );
+		}
+	}
+
+	private function validateAnalyzers() {
+		function vaActualMatchRequired( $prefix, $settings, $required ) {
+			foreach( $required as $key => $value ) {
+				$settingsKey = $prefix . '.' . $key;
+				if ( is_array( $value ) ) {
+					return vaActualMatchRequired( $settingsKey, $settings, $value );
+				}
+				// Note that I really mean !=, not !==.  Coercion is cool here.
+				if ( !array_key_exists( $settingsKey, $settings ) || $settings[ $settingsKey ] != $value ) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		$this->output( "Validating analyzers..." );
+		$settings = CirrusSearch::getIndex()->getSettings()->get();
+		$requiredAnalyzers = $this->buildAnalyzers();
+		if ( vaActualMatchRequired( 'index', $settings, $requiredAnalyzers ) ) {
+			$this->output( "ok\n" );
+		} else {
+			$this->output( "different..." );
+			$this->closeAndCorrect( function() use ($requiredAnalyzers) {
+				// TODO force the reindex.
+				// TODO do not make changes unless the user oks reindexing.
+				CirrusSearch::getIndex()->getSettings()->set( $requiredAnalyzers );
+			} );
+		}
+	}
+
+	private function validateMapping() {
+		function vmActualMatchRequired( $actual, $required ) {
+			foreach( $required as $key => $value ) {
+				if ( !array_key_exists( $key, $actual ) ) {
+					return false;
+				}
+				if ( is_array( $value ) ) {
+					if ( !is_array( $actual[ $key ] ) ) {
+						return false;
+					}
+					return vmActualMatchRequired( $actual[ $key ], $value );
+				}
+				// Note that I really mean !=, not !==.  Coercion is cool here.
+				if ( $actual[ $key ] != $value ) {
+					return false;
+				}
+			}
+			return true;
+		}
+
+		$this->output( "Validating mappings...\n" );
+		$actualMappings = CirrusSearch::getIndex()->getMapping();
+		$actualMappings = $actualMappings[ CirrusSearch::getIndex()->getName() ];
+
+		$this->output( "\tValidating mapping for page type..." );
+		$requiredPageMappings = $this->buildPageMappings();
+		if ( array_key_exists( 'page', $actualMappings) && 
+				vmActualMatchRequired( $actualMappings[ 'page' ][ 'properties' ], $requiredPageMappings ) ) {
+			$this->output( "ok\n" );
+		} else {
+			$this->output( "different..." );
+			// TODO Conflict resolution here might leave old portions of mappings
+			$action = new \Elastica\Type\Mapping( CirrusSearch::getPageType(), $requiredPageMappings );
+			$action->send();
+			$this->output( "corrected\n" );
+		}
+	}
+
+	/**
+	 * Rebuild the index by pulling everything out of it and putting it back in.  This should be faster than
+	 * reparsing everything.
+	 */
+	private function reindex() {
+		// TODO forcing a reindex causes an infinite loop right now because of what looks like a bug in Elastica.
+		global $wgCirrusSearchShardCount;
+
+		$query = new Elastica\Query();
+		$query->setFields( array( '_id', '_source' ) );
+
+		$result = CirrusSearch::getPageType()->search( $query, array(
+			'search_type' => 'scan',
+			'scroll' => '10m',
+			'size'=> $this->reindexChunkSize / $wgCirrusSearchShardCount
+		) );
+
+		while ( true ) {
+			wfProfileIn( __method__ . '::receiveDocs' );
+			$result = CirrusSearch::getIndex()->search( array(), array(
+				'scroll_id' => $result->getResponse()->getScrollId(),
+				'scroll' => '10m'
+			) );
+			wfProfileOut( __method__ . '::receiveDocs' );
+			if ( !$result->count() ) {
+				$this->output( "All done\n" );
+				break;
+			}
+			$this->output( 'Reindexing ' . $result->count() . " documents\n" );
+			wfProfileIn( __method__ . '::packageDocs' );
+			$documents = array();
+			while ( $result->current() ) {
+				$this->output( 'Got ' . $result->current()->getId() . "\n" );
+				$documents[] = new \Elastica\Document( $result->current()->getId(), $result->current()->getSource() );
+				$result->next();
+			}
+			wfProfileOut( __method__ . '::packageDocs' );
+			wfProfileIn( __method__ . '::sendDocs' );
+			try {
+				$updateResult = CirrusSearch::getPageType()->addDocuments( $documents );
+				wfDebugLog( 'CirrusSearch', 'Update completed in ' . $updateResult->getEngineTime() . ' (engine) millis' );
+			} catch ( \Elastica\Exception\Bulk\ResponseException $e ) {
+				// TODO verify this is the right exception
+				error_log( "CirrusSearch update failed caused by:  " . $e->getMessage() );
+			}
+			wfProfileOut( __method__ . '::sendDocs' );
+		}
+	}
+
+	private function createIndex( $rebuild ) {
+		global $wgCirrusSearchShardCount, $wgCirrusSearchReplicaCount;
+		CirrusSearch::getIndex()->create( array(
+			'settings' => array_merge( $this->buildAnalyzers(), array(
+				'number_of_shards' => $wgCirrusSearchShardCount,
+				'number_of_replicas' => $wgCirrusSearchReplicaCount
+			) )
+		), $rebuild );
+		$this->closeOk = false;
+	}
+
+	private function buildAnalyzers() {
+		return array(
+			'analysis' => array(
+				'analyzer' => array(
+					'text' => $this->buildTextAnalyzer()
+				)
+			)
+		);
+	}
+
+	private function buildTextAnalyzer() {
+		global $wgLanguageCode;
+		switch ($wgLanguageCode) {
+			case 'en': return array(
+				'type' => 'english'
+			);
+		}
+	}
+
+	private function buildPageMappings() {
+		return array(
+			'title' => array(
+				'type' => 'string',
+				'analyzer' => 'text'
+			),
+			'text' => array(
+				'type' => 'string',
+				'analyzer' => 'text'
+			),
+			'category' => array(
+				'type' => 'string',
+				'analyzer' => 'text'
+			)
+		);
+	}
+
+	private function closeAndCorrect( $callback ) {
+		if ( $this->closeOk ) {
+			CirrusSearch::getIndex()->close();
+			$this->closed = true;
+			$callback();
+			$this->output( "corrected\n" );
+		} else {
+			$this->output( "cannot correct\n" );
+			$this->error("This script encountered an index difference that requires that the index be\n" .
+				"closed, modified, and then reopened.  To allow this script to close the index run it\n" .
+				"with the --closeOk parameter and it'll close the index for the briefest possible time\n" .
+				"Note that the index will be unusable while closed." );
+		}
+	}
+}
+
+$maintClass = "UpdateElasticsearchIndex";
+require_once RUN_MAINTENANCE_IF_MAIN;

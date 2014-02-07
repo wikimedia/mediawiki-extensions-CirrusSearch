478075bef52d91ce5372c238ab41f47791f1515a
Nik Everett <neverett@wikimedia.org>
2014-02-07 10:17:34 -0500

    Rely on mediawiki_selenium's screenshot on failure
    This is I123a820a278b52e889f5a3dece6f4e55ad4a4c85 in the
    mediawiki/selenium repository.  We can merge this ahead of time but we'll
    lose the screenshots.
    
    Change-Id: Iaf446a43f4662db71f13c92a98c42fdfb2413f83

--- tests/browser/features/support/env.rb
+++ tests/browser/features/support/env.rb
@@ -1,12 +1,1 @@
 require "mediawiki_selenium"
-require "fileutils"
-
-After do |scenario|
-  if scenario.failed? && (ENV["SCREENSHOT_FAILURES"] == "true")
-    FileUtils.mkdir_p "screenshots"
-    name = test_name(scenario).gsub(/ /, '_')
-    path = "screenshots/#{name}.png"
-    $browser.screenshot.save path
-    embed path, "image/png"
-  end
-end

cc16a2153213ffafb6f9559df85266c066e82e5c
Zeljko Filipin <zeljko.filipin@gmail.com>
2014-02-13 16:49:42 +0100

    Moved tests from browsertests repository
    See also the commit[1] that deletes the tests from browsertests
    repository and the commit[2] in mediawiki/selenium repository that
    documents changes in Jenkins jobs.
    
    The tests run just fine on my machine. Added CREDITS file 
    following advice from Luis Villa[3].
    
    1: https://gerrit.wikimedia.org/r/#/c/113115/
    2: https://gerrit.wikimedia.org/r/#/c/113138/
    3: https://bugzilla.wikimedia.org/show_bug.cgi?id=49313#c2
    
    Bug: 61311
    Change-Id: Id293a6bb06430bd49334c2964a4c2a28e175182a

---
+++ tests/browser/features/step_definitions/simple_search_steps.rb
@@ -0,0 +1,29 @@
+#
+# This file is subject to the license terms in the COPYING file found in the
+# CirrusSearch top-level directory and at
+# https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FCirrusSearch/HEAD/COPYING. No part of
+# CirrusSearch, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the COPYING file.
+#
+# Copyright 2012-2014 by the Mediawiki developers. See the CREDITS file in the
+# CirrusSearch top-level directory and at
+# https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FCirrusSearch/HEAD/CREDITS
+#
+When(/^I search for: (.+)$/) do |search_term|
+  on(SearchPage).search_input_element.when_present.send_keys(search_term)
+end
+
+Then(/^a list of suggested pages should appear$/) do
+  on(SearchPage).search_results_element.when_present.should exist
+end
+Then(/^I should land on Search Results page$/) do
+  on(SearchResultsPage).search_element.when_present
+  @browser.url.should match Regexp.escape("&title=Special%3ASearch")
+end
+Then(/^(.+) should be the first result$/) do |page_name|
+  on(SearchPage).one_result.should == page_name
+end
+
+Then(/^the page I arrive on has title (.+)$/) do |title|
+ @browser.title.should match Regexp.escape(title)
+end

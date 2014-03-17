4089b0231ca7a61d014fcf070a642365b00f1ef9
aude <aude.wiki@gmail.com>
2014-03-17 18:02:14 +0100

    Rename HooksTest to CirrusSearchHooksTest
    to avoid naming conflicts with HooksTest in core,
    and fatal error when running core + extension tests.
    
    Change-Id: I7fa197f298e2be416dfb0ff6f7cdc65a1082a067

--- tests/unit/CirrusSearchHooksTest.php
+++ tests/unit/CirrusSearchHooksTest.php
@@ -17,7 +17,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  * http://www.gnu.org/copyleft/gpl.html
  */
-class HooksTest extends MediaWikiTestCase {
+class CirrusSearchHooksTest extends MediaWikiTestCase {
 	public function testHooksAreArrays() {
 		global $wgHooks;
 

7fc1b944a86c968fcb78a7d22a6d0bd56dd192ee
Nik Everett <neverett@wikimedia.org>
2014-04-07 15:51:42 -0400

    Faster, more stable browser tests
    Speed up browser tests by switching from a browser to the api to create pages.
    We're not testing that functionality any way.
    
    With the savings we can increase the timeouts on delayed update actions
    without feeling like we're "taking forvever".  For the most part the whole
    thing should be faster even with the increases.
    
    Needs Id0eb49fe3ac1aee3d320e9a1a6a52c7d89f7e2b9 before it'll actually work.
    
    Change-Id: Ic5290e1d811a5634fc4111da3d2b14479dec5d5a

--- tests/browser/features/step_definitions/page_steps.rb
+++ tests/browser/features/step_definitions/page_steps.rb
@@ -11,14 +11,17 @@ Given(/^a file named (.*) exists with contents (.*) and description (.*)$/) do |
 end
 
 Given(/^a page named (.*) doesn't exist$/) do |title|
-  visit(ArticlePage, using_params: {page_name: title}) do |page|
-    (page.create_source_link? or page.create_link?).should be_true
-  end
+  step("I delete #{title}")
 end
 
 When(/^I delete (?!the second)(.+)$/) do |title|
-  visit(DeletePage, using_params: {page_name: title}) do |page|
-    page.delete
+  require "mediawiki_api"
+  client = MediawikiApi::Client.new("#{ENV['MEDIAWIKI_URL']}../w/api.php", false)
+  client.log_in(ENV['MEDIAWIKI_USER'], ENV['MEDIAWIKI_PASSWORD'])
+  result = client.delete_page(title, "Testing")
+  result.status.should eq 200
+  if !result.body.include?("missingtitle") then
+    result.body.should_not include '{"error":{"code"'
   end
 end
 When(/^I edit (.+) to add (.+)$/) do |title, text|
@@ -46,34 +49,24 @@ def edit_page(title, text, add)
   if text.start_with?("@")
     text = File.read("features/support/articles/" + text[1..-1])
   end
-  visit(EditPage, using_params: {page_name: title}) do |page|
-    if (!page.article_text? and page.login?) then
-      # Looks like we're not being given the article text probably because we're
-      # trying to edit an article that requires us to be logged in.  Lets try
-      # logging in.
-      step "I am logged in"
-      visit(EditPage, using_params: {page_name: title})
-    end
-    if (page.article_text.strip != text.strip) then
-      if (!page.save? and page.login?) then
-        # Looks like I'm at a page I don't have permission to change and I'm not
-        # logged in.  Lets log in and try again.
-        step "I am logged in"
-        visit(EditPage, using_params: {page_name: title})
-      end
-      if !add then
-        page.article_text = ""
-      end
-      # Firefox chokes on huge batches of text so split it into chunks and use
-      # send_keys rather than page-objects built in += because that clears and
-      # resends everything....
-
-      # Note that this doens't work with tabs!!!!!!!
-      text.chars.each_slice(1000) do |chunk|
-        page.article_text_element.send_keys(chunk)
-      end
-      page.save
-    end
+  require "mediawiki_api"
+  client = MediawikiApi::Client.new("#{ENV['MEDIAWIKI_URL']}../w/api.php", false)
+  fetched_text = client.get_wikitext(title)
+  if fetched_text.status == 404 then
+    fetched_text = ""
+  else
+    fetched_text.status.should eq 200
+    fetched_text = fetched_text.body.strip
+  end
+  if (add)
+    # Note that the space keeps from jamming words together
+    text = fetched_text + " " + text
+  end
+  if fetched_text.strip != text.strip then
+    client.log_in(ENV['MEDIAWIKI_USER'], ENV['MEDIAWIKI_PASSWORD'])
+    result = client.create_page(title, text)
+    result.status.should eq 200
+    result.body.should_not include '{"error":{"code"'
   end
 end
 
--- tests/browser/features/update_general.feature
+++ tests/browser/features/update_general.feature
@@ -1,34 +1,34 @@
 @clean
 Feature: Search backend updates
-  Background:
-    Given I am logged in
-
   Scenario: Deleted pages are removed from the index
     Given a page named DeleteMe exists
-    Then within 10 seconds searching for DeleteMe yields DeleteMe as the first result
+    And I am at a random page
+    Then within 20 seconds searching for DeleteMe yields DeleteMe as the first result
     When I delete DeleteMe
-    Then within 10 seconds searching for DeleteMe yields none as the first result
+    Then within 20 seconds searching for DeleteMe yields none as the first result
 
   Scenario: Altered pages are updated in the index
     Given a page named ChangeMe exists with contents foo
     When I edit ChangeMe to add superduperchangedme
-    Then within 10 seconds searching for superduperchangedme yields ChangeMe as the first result
+    And I am at a random page
+    Then within 20 seconds searching for superduperchangedme yields ChangeMe as the first result
 
   Scenario: Pages containing altered template are updated in the index
     Given a page named Template:ChangeMe exists with contents foo
     And a page named ChangeMyTemplate exists with contents {{Template:ChangeMe}}
     When I edit Template:ChangeMe to add superduperultrachangedme
-    # Updating a template uses the job queue and that can take quite a while to complete in beta
-    Then within 10 seconds searching for superduperultrachangedme yields ChangeMyTemplate as the first result
+    And I am at a random page
+    Then within 20 seconds searching for superduperultrachangedme yields ChangeMyTemplate as the first result
 
   # This test doesn't rely on our paranoid revision delete handling logic, rather, it verifies what should work with the
   # logic with a similar degree of paranoia
   Scenario: When a revision is deleted the page is updated regardless of if the revision is current
-    Given a page named RevDelTest exists with contents first
+    Given I am logged in 
+    And a page named RevDelTest exists with contents first
     And a page named RevDelTest exists with contents delete this revision
-    And within 10 seconds searching for intitle:RevDelTest "delete this revision" yields RevDelTest as the first result
+    And within 20 seconds searching for intitle:RevDelTest "delete this revision" yields RevDelTest as the first result
     And a page named RevDelTest exists with contents current revision
     When I delete the second most recent revision of RevDelTest
-    Then within 10 seconds searching for intitle:RevDelTest "delete this revision" yields none as the first result
+    Then within 20 seconds searching for intitle:RevDelTest "delete this revision" yields none as the first result
     When I search for intitle:RevDelTest current revision
     Then RevDelTest is the first search result

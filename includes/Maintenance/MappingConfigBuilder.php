99f07266231a0bf6643c1e995f7a5719fe813960
Nik Everett <neverett@wikimedia.org>
2014-09-08 08:58:49 -0400

    Store weighted all_near_match with freqs
    We need the freqs to calculate weight properly.
    
    Some comments too.
    
    Change-Id: Ifa142233134c77d077920fb29f3df7f54540cf8e

--- includes/Maintenance/MappingConfigBuilder.php
+++ includes/Maintenance/MappingConfigBuilder.php
@@ -181,7 +181,7 @@ class MappingConfigBuilder {
 			$config[ 'properties' ][ 'all_near_match' ] = array(
 				'type' => 'string',
 				'analyzer' => 'near_match',
-				'index_options' => 'docs',
+				'index_options' => 'freqs',
 				'position_offset_gap' => self::POSITION_OFFSET_GAP,
 				'norms' => array( 'enabled' => false ),
 			);
@@ -197,7 +197,7 @@ class MappingConfigBuilder {
 	}
 
 	/**
-	 * Setup copy_to for some fields to $destionation.
+	 * Setup copy_to for some fields to $destination.
 	 * @param array $config to modify
 	 * @param array $fields field name to number of times copied
 	 * @param string $destination destination of the copy
@@ -205,6 +205,8 @@ class MappingConfigBuilder {
 	 */
 	private function setupCopyTo( $config, $fields, $destination ) {
 		foreach ( $fields as $field => $weight ) {
+			// Note that weights this causes weights that are not whole numbers to be rounded up.
+			// We're ok with that because we don't have a choice.
 			for ( $r = 0; $r < $weight; $r++ ) {
 				if ( $field === 'redirect' ) {
 					// Redirect is in a funky place

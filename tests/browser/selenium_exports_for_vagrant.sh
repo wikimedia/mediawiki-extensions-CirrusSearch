ed4bdb31f6d2643810c91431ee0fb488ad0b32ca
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-04-22 16:21:20 -0700

    Update browsertest environment for vagrant changes
    I7584698d updates mw-vagrant to contain the various cirrus hacks
    to a cirrus specific wiki.  This updates the environment to point
    to that wiki
    
    Bug: T87296
    Change-Id: I47e5423510fbcf39ba2d154028addc40f06ecde4

--- tests/browser/selenium_exports_for_vagrant.sh
+++ tests/browser/selenium_exports_for_vagrant.sh
@@ -1,7 +1,7 @@
 export MEDIAWIKI_USER=admin
 export MEDIAWIKI_PASSWORD=vagrant
-export MEDIAWIKI_URL=http://127.0.0.1:8080/wiki/
-export MEDIAWIKI_API_URL=http://127.0.0.1:8080/w/api.php
+export MEDIAWIKI_URL=http://cirrustest.wiki.local.wmftest.net:8080/wiki/
+export MEDIAWIKI_API_URL=http://cirrustest.wiki.local.wmftest.net:8080/w/api.php
 export REUSE_BROWSER=true
 export SCREENSHOT_FAILURES=true
 export BROWSER=phantomjs

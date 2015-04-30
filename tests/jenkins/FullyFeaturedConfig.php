dc92e3a6f76734e86060da21eb4e3d3a16f4d377
Nik Everett <neverett@wikimedia.org>
2015-04-30 13:34:05 -0400

    Split Jenkins.php into two files
    Jenkins.php contains any crazy configuration required for fully running
    integration tests. FullyFeaturedConfig.php contains configuration that gets
    the wiki using Cirrus with the bells and whistles that WMF uses in production
    like the redis job queue and some Elasticsearch plugins designed for use
    with Cirrus. They make using Cirrus much nicer.
    
    Change-Id: I855b7c419dedcabd58d2ec0fa5ce0ef9f08d7651

---
+++ tests/jenkins/FullyFeaturedConfig.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * Sets up decently fully features cirrus configuration that relies on some of
+ * the stuff installed by MediaWiki-Vagrant.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+require_once( "$IP/extensions/Elastica/Elastica.php" );
+
+
+$wgSearchType = 'CirrusSearch';
+$wgCirrusSearchUseExperimentalHighlighter = true;
+$wgCirrusSearchOptimizeIndexForExperimentalHighlighter = true;
+$wgCirrusSearchWikimediaExtraPlugin[ 'regex' ] = array( 'build', 'use' );
+$wgCirrusSearchWikimediaExtraPlugin[ 'safer' ] = array(
+	'phrase' => array(
+	)
+);
+$wgCirrusSearchWikimediaExtraPlugin[ 'super_detect_noop' ] = true;
+
+
+if ( class_exists( 'PoolCounter_Client' ) ) {
+	// If the pool counter is around set up prod like pool counter settings
+	$wgPoolCounterConf[ 'CirrusSearch-Search' ] = array(
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 432,
+		'maxqueue' => 600,
+	);
+	// Super common and mostly fast
+	$wgPoolCounterConf[ 'CirrusSearch-Prefix' ] = array(
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 432,
+		'maxqueue' => 600,
+	);
+	// Regex searches are much heavier then regular searches so we limit the
+	// concurrent number.
+	$wgPoolCounterConf[ 'CirrusSearch-Regex' ] = array(
+		'class' => 'PoolCounter_Client',
+		'timeout' => 60,
+		'workers' => 10,
+		'maxqueue' => 20,
+	);
+	// These should be very very fast and reasonably rare
+	$wgPoolCounterConf[ 'CirrusSearch-NamespaceLookup' ] = array(
+		'class' => 'PoolCounter_Client',
+		'timeout' => 5,
+		'workers' => 50,
+		'maxqueue' => 200,
+	);
+	// Can't be enabled until poolcounter gets Ie282b8486c7bad451fbc5fb9a8274c6e01a728a7.
+	// TODO enable this.
+	// $wgPoolCounterConf[ 'CirrusSearch-PerUser' ] = array(
+	// 	'class' => 'PoolCounter_Client',
+	// 	'timeout' => 0,
+	// 	'workers' => 1,
+	// 	'maxqueue' => 1,
+	// );
+}

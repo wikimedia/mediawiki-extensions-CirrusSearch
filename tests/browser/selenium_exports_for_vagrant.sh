e1bcae2c4a0f654a0c240450f2280c1c645319f8
Erik Bernhardson <ebernhardson@wikimedia.org>
2015-08-13 12:53:20 -0700

    pick right urls to run browser test against
    When running vagrant in a container in labs these urls need
    to point to the wmflabs.org domain, not wmftest.net.
    
    This doesn't completely fix the problem, the container gets the
    wrong hostname. Another patch will be needed for mwv.
    
    Change-Id: Ie9f19a777671fdf29cf688d35a041e5376483e9b

--- tests/browser/selenium_exports_for_vagrant.sh
+++ tests/browser/selenium_exports_for_vagrant.sh
@@ -1,9 +1,16 @@
 export MEDIAWIKI_USER=admin
 export MEDIAWIKI_PASSWORD=vagrant
-export MEDIAWIKI_URL=http://cirrustest.wiki.local.wmftest.net:8080/wiki/
-export MEDIAWIKI_API_URL=http://cirrustest.wiki.local.wmftest.net:8080/w/api.php
-export MEDIAWIKI_COMMONS_API_URL=http://commons.wiki.local.wmftest.net:8080/w/api.php
 export REUSE_BROWSER=true
 export SCREENSHOT_FAILURES=true
 export BROWSER=phantomjs
 export HEADLESS=true
+
+if [ -d /srv/mediawiki-vagrant ]; then
+  export MEDIAWIKI_URL=http://cirrustest-${HOSTNAME}.wmflabs.org:8080/wiki/
+  export MEDIAWIKI_API_URL=http://cirrustest-${HOSTNAME}.wmflabs.org:8080/w/api.php
+  export MEDIAWIKI_COMMONS_API_URL=http://commons-${HOSTNAME}.wmflabs.org:8080/w/api.php
+else
+  export MEDIAWIKI_URL=http://cirrustest.wiki.local.wmftest.net:8080/wiki/
+  export MEDIAWIKI_API_URL=http://cirrustest.wiki.local.wmftest.net:8080/w/api.php
+  export MEDIAWIKI_COMMONS_API_URL=http://commons.wiki.local.wmftest.net:8080/w/api.php
+fi

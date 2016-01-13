c895d0f37dc2e4ebb3be4a02a2f6d75412e70782
dcausse <dcausse@wikimedia.org>
2016-01-12 17:21:27 -0800

    Break CirrusSearch request times by query type
    Can be useful in top percentile graphs.
    Also set "namespace" as queryType when doing a namespace lookup.
    
    Change-Id: I706f0771436426795441865d2d007c375c5b6f3a

--- includes/Searcher.php
+++ includes/Searcher.php
@@ -921,6 +921,8 @@ GROOVY;
 				try {
 					$searcher->start( "lookup namespace for {namespaceName}", array(
 						'namespaceName' => $name,
+						'query' => $name,
+						'queryType' => 'namespace',
 					) );
 					$pageType = $conn->getNamespaceType( $indexBaseName );
 					$match = new \Elastica\Query\Match();

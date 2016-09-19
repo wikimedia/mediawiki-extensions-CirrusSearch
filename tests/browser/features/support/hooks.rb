7c6d92423facfd5de2eefcd3ab65d1a4bda427f4
dcausse <dcausse@wikimedia.org>
2016-09-19 14:36:48 +0200

    Increase wait times for oncommons browser tests
    Change-Id: Ic0f266af559086321283ef7348fa5ce42f5a0fda

--- tests/browser/features/support/hooks.rb
+++ tests/browser/features/support/hooks.rb
@@ -74,12 +74,12 @@ setup_commons = lambda do |world|
       Given I delete on commons File:OnCommons.svg
       And I delete on commons File:DuplicatedLocally.svg
       And I delete File:DuplicatedLocally.svg
-      And I wait 5 seconds
+      And I wait 10 seconds
       And a file named File:OnCommons.svg exists on commons with contents OnCommons.svg and description File stored on commons for test purposes
       And a file named File:DuplicatedLocally.svg exists on commons with contents DuplicatedLocally.svg and description File stored on commons and duplicated locally
-      And I wait 5 seconds
+      And I wait 10 seconds
       And a file named File:DuplicatedLocally.svg exists with contents DuplicatedLocally.svg and description Locally stored file duplicated on commons
-      And I wait 5 seconds
+      And I wait 10 seconds
         )
     commons = true
   end

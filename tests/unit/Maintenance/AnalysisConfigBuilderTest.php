66c6ece1d3179fd7167baab523ce8ad765a51944
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-04-04 11:46:17 -0700

    Add fixtures for ukrainian analysis config
    It looks like ukrainian was missed when building up the analysis
    chain fixtures. Add it in to ease reviewing future changes to
    analysis chains.
    
    Change-Id: I41e2519fa16f7a3fbbf21e23df306c174b18c3e5

--- tests/unit/Maintenance/AnalysisConfigBuilderTest.php
+++ tests/unit/Maintenance/AnalysisConfigBuilderTest.php
@@ -395,6 +395,7 @@ class AnalysisConfigBuilderTest extends CirrusTestCase {
 		$plugins = [
 			'analysis-stempel', 'analysis-kuromoji',
 			'analysis-smartcn', 'elasticsearch-analysis-hebrew',
+			'analysis-ukrainian',
 		];
 		$builder = new AnalysisConfigBuilder( $langCode, $plugins, $config );
 		if ( is_string( $expected ) ) {

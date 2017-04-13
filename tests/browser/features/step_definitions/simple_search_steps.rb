0c9fe6855eef4b4bdb77f3ceb8e78a38b9911c44
Stanislav Malyshev <smalyshev@gmail.com>
2017-04-13 12:50:56 -0700

    Temporarily disable smoke:16
    Looks like this is the only place we try to show suggestion box, and it doesn't work.
    To reduce noise, after manually testing suggestion box is not actually broken, let us
    disable it and look for why it may be failing.
    
    The patch also cleans up some duplicate code and weird similar, but not quite identical
    scenario strings.
    
    Change-Id: Id1420ae00c5b37b143ce52ee9907ca3392001e0c

--- tests/browser/features/step_definitions/simple_search_steps.rb
+++ tests/browser/features/step_definitions/simple_search_steps.rb
@@ -9,24 +9,10 @@
 # CirrusSearch top-level directory and at
 # https://git.wikimedia.org/blob/mediawiki%2Fextensions%2FCirrusSearch/HEAD/CREDITS
 #
-When(/^I search for: (.+)$/) do |search_term|
-  on(SearchPage).search_input_element.when_present.send_keys(search_term)
-end
-
-Then(/^a list of suggested pages should appear$/) do
-  with_browser do
-    on(SearchPage).search_results_element.when_present.should exist
-  end
-end
 Then(/^I should land on Search Results page$/) do
   with_browser do
     on(SearchResultsPage).search_element.when_present
     browser.url.should match Regexp.escape("&title=Special%3ASearch")
-  end
-end
-Then(/^(.+) should be the first result$/) do |page_name|
-  with_browser do
-    on(SearchPage).first_result.should == page_name
   end
 end
 

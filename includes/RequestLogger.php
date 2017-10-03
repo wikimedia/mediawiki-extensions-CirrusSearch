c75c0cbd881865ce4bbe5680a149e823e41d7bd6
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-10-03 15:51:34 -0700

    Add additional metadata for detecting automated/bot requests to search logs
    Our search logs contain a bunch of information, but figuring out what
    part of it is explicitly tied to usage on our sites vs external usages
    is difficult at best. Try and help out a little by including information
    on what referred the search request.
    
    * referer - The presence or absense of this can be useful information.
      Additionally if the referer points to a site other than where the
      request is running that can also be useful information
    
    * hascookies - Lots of automated http doesn't bother to track cookies.
     At least within WMF users will have a cookie set on visit that
     indicates the last day they visited the site, and we expect to see
     that when they submit a search/autocomplete form. If not present that
     can be useful information for bot filtering.
    
    Change-Id: If58ed7f9a89f9604873358564a530b90f3a8aac7

--- includes/RequestLogger.php
+++ includes/RequestLogger.php
@@ -262,6 +262,12 @@ class RequestLogger {
 				// When tracking down performance issues it is useful to know if they are localized
 				// to a particular set of instances
 				'host' => gethostname(),
+				// Referer can be helpful when trying to figure out what requests were made by bots.
+				'referer' => (string)( $wgRequest->getHeader( 'Referer' ) ?: '' ),
+				// Reasonable indication it's not a "normal" web request, as a standard web request
+				// would have first had a WMF-Last-Access cookie set, and then submitting an
+				// autocomplete/fulltext search would have sent that as well.
+				'hascookies' => $wgRequest->getHeader( 'Cookie' ) ? "1" : "0",
 			],
 			'requests' => $requests,
 		];

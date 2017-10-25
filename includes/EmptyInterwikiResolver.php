e8de30e4536138e14d41bb1b6715ab7d0a28fdd3
Erik Bernhardson <ebernhardson@wikimedia.org>
2017-10-25 14:57:05 -0700

    EmptyInterwikiResolver doesn't fully implement InterwikiResolver
    Not sure how this made it through CI, but tests no longer
    pass because this interface isn't fuly implemented. Update
    the implementation for recent changes.
    
    Change-Id: I922aa64a5394b335aa28d2d112d72ae9457460e4

--- includes/EmptyInterwikiResolver.php
+++ includes/EmptyInterwikiResolver.php
@@ -11,6 +11,13 @@ class EmptyInterwikiResolver implements InterwikiResolver {
 	}
 
 	/**
+	 * @return SearchConfig[] configs of sister projects indexed by interwiki prefix
+	 */
+	public function getSisterProjectConfigs() {
+		return [];
+	}
+
+	/**
 	 * @param string $wikiId
 	 * @return string|null the interwiki identified for this $wikiId or null if none found
 	 */
@@ -23,6 +30,14 @@ class EmptyInterwikiResolver implements InterwikiResolver {
 	 * @return string[] a single elt array [ 'iw_prefix' => 'wikiId' ] or [] if none found
 	 */
 	public function getSameProjectWikiByLang( $lang ) {
+		return [];
+	}
+
+	/**
+	 * @param string $lang
+	 * @return SearchConfig[] zero or one element array: [] or [ interwiki -> SearchConfig ]
+	 */
+	public function getSameProjectConfigByLang( $lang ) {
 		return [];
 	}
 }

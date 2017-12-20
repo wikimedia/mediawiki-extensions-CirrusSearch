07546541bd316c8217a549daf0aab4af21f4c200
Florianschmidtwelzow <florian.schmidt.stargatewissen@gmail.com>
2017-12-20 01:28:53 +0000

    Revert "Fail gracefully if curl is not installed"
    Will be moved to I98db51243f9e94c09e007bdd04b0df308d4298e9
    
    This reverts commit cf2025b5a6f380de43509ac0a5738cc67de85cb1.
    
    Change-Id: I83e9a7c33b988fb1a148bc3d6baf23a62c49818b

--- CirrusSearch.php
+++ CirrusSearch.php
@@ -1280,11 +1280,6 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 }
 
 /**
- * Check if curl is installed
- */
-$wgExtensionFunctions[] = 'CirrusSearch\Hooks::checkCurlInstallation';
-
-/**
  * Hooks
  */
 $wgHooks[ 'CirrusSearchBuildDocumentFinishBatch'][] = 'CirrusSearch\BuildDocument\RedirectsAndIncomingLinks::finishBatch';

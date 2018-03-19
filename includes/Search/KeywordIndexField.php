47eb03567f6cbea092a69ff67515591928d15314
Stanislav Malyshev <smalyshev@gmail.com>
2018-03-19 14:41:08 -0700

    Since we decided not to use native keyword type, remove TODOs about it.
    Change-Id: I488c5769bb6b89a0213d0bdebf1c34ada2a2b5a0

--- includes/Search/KeywordIndexField.php
+++ includes/Search/KeywordIndexField.php
@@ -8,6 +8,11 @@ namespace CirrusSearch\Search;
  * @package CirrusSearch
  */
 class KeywordIndexField extends CirrusIndexField {
+	/**
+	 * Using text type here since it's better for our purposes than native
+	 * keyword type.
+	 * @var string
+	 */
 	protected $typeName = 'text';
 
 	/**
@@ -16,19 +21,13 @@ class KeywordIndexField extends CirrusIndexField {
 	const KEYWORD_IGNORE_ABOVE = 5000;
 
 	public function getMapping( \SearchEngine $engine ) {
-		// TODO: Should we vary between keyword and text type? For now using text
-		// because keyword doesn't support analyzers for lowercasing.
 		$config = parent::getMapping( $engine );
-		// TODO with keyword type in ES 5.2 this will become 'normalizer'
 		$config['analyzer'] =
 			$this->checkFlag( self::FLAG_CASEFOLD ) ? 'lowercase_keyword' : 'keyword';
 		$config += [
 			'norms' => false,
 			// Omit the length norm because there is only even one token
 			'index_options' => 'docs',
-			// TODO: Re-enable after upgrade to es 5.2 and changing type to keyword
-			// Omit the frequency and position information because neither are useful
-			// 'ignore_above' => self::KEYWORD_IGNORE_ABOVE,
 		];
 		return $config;
 	}

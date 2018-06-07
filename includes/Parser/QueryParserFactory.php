094449d8105723d5ba8ea6298d93d9015d43c2ee
David Causse <dcausse@wikimedia.org>
2018-06-07 18:23:16 +0200

    Add a very simple QueryParserFactory
    Change-Id: I61f1f90a238799a13430717d05fd5eb9c7c05275

---
+++ includes/Parser/QueryParserFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CirrusSearch\Parser;
+
+use CirrusSearch\Parser\QueryStringRegex\QueryStringRegexParser;
+use CirrusSearch\Search\Escaper;
+use CirrusSearch\SearchConfig;
+
+/**
+ * Simple factory to create QueryParser instance based on the host wiki config.
+ * @see QueryParser
+ */
+class QueryParserFactory {
+
+	/**
+	 * Get the default fulltext parser.
+	 * @param SearchConfig $config the host wiki config
+	 * @return QueryParser
+	 */
+	public static function newFullTextQueryParser( SearchConfig $config ) {
+		$escaper = new Escaper( $config->get( 'LanguageCode' ), $config->get( 'CirrusSearchAllowLeadingWildcard' ) );
+		return new QueryStringRegexParser( new FullTextKeywordRegistry( $config ),
+			$escaper, $config->get( 'CirrusSearchStripQuestionMarks' ) );
+	}
+}

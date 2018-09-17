49fd0565ace89020976a35fe4d0368df15268285
David Causse <dcausse@wikimedia.org>
2018-09-17 16:12:25 +0200

    Drop support for elasticsearch-langdetect plugin
    Change-Id: Idc248dcbd5c5c3ba7a61c4a9176ff6070a092f87

--- CirrusSearch.php
+++ CirrusSearch.php
@@ -1121,7 +1121,6 @@ $wgCirrusSearchInterwikiThreshold = 3;
  *
  * CirrusSearch\LanguageDetector\HttpAccept - uses the first language in the
  *  Accept-Language header that is not the current content language.
- * CirrusSearch\LanguageDetector\ElasticSearch - uses the elasticsearch lang-detect plugin
  * CirrusSearch\LanguageDetector\TextCat - uses TextCat library
  */
 $wgCirrusSearchLanguageDetectors = [];

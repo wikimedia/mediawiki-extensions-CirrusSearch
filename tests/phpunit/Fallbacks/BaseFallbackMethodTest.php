wikimedia/mediawiki-extensions-CirrusSearch

commit 4ca45659187e4b6261e0dd219862b98f42cf20b4

Generalize fallback & second-try methods
Add a FallbackMethod to implement a fallback/retry strategy. 2 methods
added so far:
- phrase suggest for rewritting using the DYM suggestion
- language detection for appending new results using cross language
  search

The class FallbackRunner is responsible for running all these methods.
For now the default strategies are hardcoded:
- rewrite using DYM
- append if the previous results returned fewer results than the
  threshold set.

In the future it would be nice to extend the FallbackMethod to allow
it to amend the Query sent to elastic (move the code to build the
phrase suggest query into PhraseSuggestFallbackMethodTest), but also
move the code that extract the DYM from the elastic response.

Bug: T204255
Change-Id: I9f661d0b8d3618bff328f4fc31d3a874458c9a12

autoload.php
@@ -42,6 +42,7 @@ $wgAutoloadClasses += [
 	'CirrusSearch\\Connection' => __DIR__ . '/includes/Connection.php',
 	'CirrusSearch\\CrossSearchStrategy' => __DIR__ . '/includes/CrossSearchStrategy.php',
 	'CirrusSearch\\DataSender' => __DIR__ . '/includes/DataSender.php',
+	'CirrusSearch\\DummyResultSet' => __DIR__ . '/tests/phpunit/DummyResultSet.php',
 	'CirrusSearch\\Dump' => __DIR__ . '/includes/Dump.php',
 	'CirrusSearch\\ElasticaErrorHandler' => __DIR__ . '/includes/ElasticaErrorHandler.php',
 	'CirrusSearch\\Elastica\\LtrQuery' => __DIR__ . '/includes/Elastica/LtrQuery.php',
@@ -58,6 +59,12 @@ $wgAutoloadClasses += [
 	'CirrusSearch\\Extra\\Query\\SourceRegex' => __DIR__ . '/includes/Extra/Query/SourceRegex.php',
 	'CirrusSearch\\Extra\\Query\\TermFreq' => __DIR__ . '/includes/Extra/Query/TermFreq.php',
 	'CirrusSearch\\Extra\\Query\\TokenCountRouter' => __DIR__ . '/includes/Extra/Query/TokenCountRouter.php',
+	'CirrusSearch\\Fallbacks\\BaseFallbackMethodTest' => __DIR__ . '/tests/phpunit/Fallbacks/BaseFallbackMethodTest.php',
+	'CirrusSearch\\Fallbacks\\FallbackMethod' => __DIR__ . '/includes/Fallbacks/FallbackMethod.php',
+	'CirrusSearch\\Fallbacks\\FallbackRunner' => __DIR__ . '/includes/Fallbacks/FallbackRunner.php',
+	'CirrusSearch\\Fallbacks\\LangDetectFallbackMethod' => __DIR__ . '/includes/Fallbacks/LangDetectFallbackMethod.php',
+	'CirrusSearch\\Fallbacks\\PhraseSuggestFallbackMethod' => __DIR__ . '/includes/Fallbacks/PhraseSuggestFallbackMethod.php',
+	'CirrusSearch\\Fallbacks\\SearcherFactory' => __DIR__ . '/includes/Fallbacks/SearcherFactory.php',
 	'CirrusSearch\\ForceSearchIndex' => __DIR__ . '/maintenance/forceSearchIndex.php',
 	'CirrusSearch\\HashSearchConfig' => __DIR__ . '/includes/HashSearchConfig.php',
 	'CirrusSearch\\Hooks' => __DIR__ . '/includes/Hooks.php',

includes/Fallbacks/LangDetectFallbackMethod.php
@@ -1,0 +1,172 @@
+<?php
+
+namespace CirrusSearch\Fallbacks;
+
+use CirrusSearch\InterwikiResolver;
+use CirrusSearch\LanguageDetector\Detector;
+use CirrusSearch\LanguageDetector\LanguageDetectorFactory;
+use CirrusSearch\Parser\BasicQueryClassifier;
+use CirrusSearch\Search\ResultSet;
+use CirrusSearch\Search\SearchMetricsProvider;
+use CirrusSearch\Search\SearchQuery;
+use CirrusSearch\Search\SearchQueryBuilder;
+use CirrusSearch\SearchConfig;
+use CirrusSearch\Searcher;
+use MediaWiki\MediaWikiServices;
+use Wikimedia\Assert\Assert;
+
+class LangDetectFallbackMethod implements FallbackMethod, SearchMetricsProvider {
+
+	/**
+	 * @var SearchQuery
+	 */
+	private $query;
+
+	/**
+	 * @var SearcherFactory
+	 */
+	private $searcherFactory;
+
+	/**
+	 * @var array|null
+	 */
+	private $searchMetrics = [];
+
+	/**
+	 * @var Detector[]
+	 */
+	private $detectors;
+
+	/**
+	 * @var InterwikiResolver
+	 */
+	private $interwikiResolver;
+
+	/**
+	 * @var SearchConfig|null
+	 */
+	private $detectedLangWikiConfig;
+
+	/**
+	 * @var int
+	 */
+	private $threshold;
+
+	/**
+	 * LangDetectFallbackMethod constructor.
+	 * @param SearchQuery $query
+	 * @param SearcherFactory $searcherFactory
+	 * @param Detector[] $detectors
+	 * @param InterwikiResolver|null $interwikiResolver
+	 */
+	public function __construct(
+		SearchQuery $query,
+		SearcherFactory $searcherFactory,
+		array $detectors,
+		InterwikiResolver $interwikiResolver = null
+	) {
+		$this->query = $query;
+		$this->searcherFactory = $searcherFactory;
+		$this->detectors = $detectors;
+		$this->interwikiResolver =
+			$interwikiResolver ??
+			MediaWikiServices::getInstance()->getService( InterwikiResolver::SERVICE );
+		$this->threshold = $query->getSearchConfig()->get( 'CirrusSearchInterwikiThreshold' );
+	}
+
+	/**
+	 * @param SearcherFactory $factory
+	 * @param SearchQuery $query
+	 * @param \WebRequest $request
+	 * @return FallbackMethod
+	 */
+	public static function build( SearcherFactory $factory, SearchQuery $query, \WebRequest $request ) {
+		$langDetectFactory = new LanguageDetectorFactory( $query->getSearchConfig(), $request );
+		return new self( $query, $factory, $langDetectFactory->getDetectors() );
+	}
+
+	/**
+	 * @param ResultSet $firstPassResults
+	 * @return float
+	 */
+	public function successApproximation( ResultSet $firstPassResults ) {
+		if ( !$this->query->getCrossSearchStrategy()->isCrossLanguageSearchSupported() ) {
+			return 0.0;
+		}
+		if ( $firstPassResults->numRows() >= $this->threshold ) {
+			return 0.0;
+		}
+		if ( !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
+			return 0.0;
+		}
+		foreach ( $this->detectors as $name => $detector ) {
+			$lang = $detector->detect( $this->query->getParsedQuery()->getRawQuery() );
+			if ( $lang === null ) {
+				continue;
+			}
+			if ( $lang === $this->query->getSearchConfig()->get( 'LanguageCode' ) ) {
+				// The query is in the wiki language so we
+				// don't need to actually try another wiki.
+				// Note that this may not be very accurate for
+				// wikis that use deprecated language codes
+				// but the interwiki resolver should not return
+				// ourselves.
+				continue;
+			}
+			$iwPrefixAndConfig = $this->interwikiResolver->getSameProjectConfigByLang( $lang );
+			if ( !empty( $iwPrefixAndConfig ) ) {
+				// it might be more accurate to attach these to the 'next'
+				// log context? It would be inconsistent with the
+				// langdetect => false condition which does not have a next
+				// request though.
+				Searcher::appendLastLogPayload( 'langdetect', $name );
+				$prefix = key( $iwPrefixAndConfig );
+				$config = $iwPrefixAndConfig[$prefix];
+				$metric = [ $config->getWikiId(), $prefix ];
+				$this->searchMetrics['wgCirrusSearchAltLanguage'] = $metric;
+				$this->detectedLangWikiConfig = $config;
+				return 0.5;
+			}
+		}
+		Searcher::appendLastLogPayload( 'langdetect', 'failed' );
+		return 0.0;
+	}
+
+	/**
+	 * @param ResultSet $firstPassResults
+	 * @param ResultSet $previousSet
+	 * @return ResultSet
+	 */
+	public function rewrite( ResultSet $firstPassResults, ResultSet $previousSet ) {
+		Assert::precondition( $this->detectedLangWikiConfig !== null,
+			'nothing has been detected, this should not even be tried.' );
+
+		if ( $previousSet->numRows() >= $this->threshold ||
+			 !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
+			return $previousSet;
+		}
+
+		$crossLangQuery = SearchQueryBuilder::forCrossLanguageSearch( $this->detectedLangWikiConfig,
+			$this->query )->build();
+		$searcher = $this->searcherFactory->makeSearcher( $crossLangQuery );
+		$status = $searcher->search( $crossLangQuery );
+		if ( !$status->isOK() ) {
+			return $previousSet;
+		}
+		$crossLangResults = $status->getValue();
+		if ( !$crossLangResults instanceof ResultSet ) {
+			// NOTE: Can/should this happen?
+			return $previousSet;
+		}
+		$this->searchMetrics['wgCirrusSearchAltLanguageNumResults'] = $crossLangResults->numRows();
+		if ( $crossLangResults->numRows() > 0 ) {
+			$previousSet->addInterwikiResults( $crossLangResults,
+				\SearchResultSet::INLINE_RESULTS, $this->detectedLangWikiConfig->getWikiId() );
+		}
+		return $previousSet;
+	}
+
+	public function getMetrics() {
+		return $this->searchMetrics;
+	}
+}

includes/Fallbacks/PhraseSuggestFallbackMethod.php
@@ -1,0 +1,92 @@
+<?php
+
+namespace CirrusSearch\Fallbacks;
+
+use CirrusSearch\Parser\BasicQueryClassifier;
+use CirrusSearch\Search\ResultSet;
+use CirrusSearch\Search\SearchQuery;
+use CirrusSearch\Search\SearchQueryBuilder;
+
+/**
+ * Fallback method based the tlastic phrase suggester.
+ * TODO: This is currently only handling the "rewrite" process using the suggestion
+ * but in the future it'll be responsible from setting the suggestion in the ResultSet
+ */
+class PhraseSuggestFallbackMethod implements FallbackMethod {
+
+	/**
+	 * @var SearchQuery
+	 */
+	private $query;
+
+	/**
+	 * @var SearcherFactory
+	 */
+	private $searcherFactory;
+
+	/**
+	 * PhraseSuggestFallbackMethod constructor.
+	 * @param SearcherFactory $factory
+	 * @param SearchQuery $query
+	 */
+	public function __construct( SearcherFactory $factory, SearchQuery $query ) {
+		$this->searcherFactory = $factory;
+		$this->query = $query;
+	}
+
+	/**
+	 * @param SearcherFactory $factory
+	 * @param SearchQuery $query
+	 * @param \WebRequest $request
+	 * @return FallbackMethod
+	 */
+	public static function build( SearcherFactory $factory, SearchQuery $query, \WebRequest $request ) {
+		return new self( $factory, $query );
+	}
+
+	/**
+	 * @param ResultSet $firstPassResults
+	 * @return float
+	 */
+	public function successApproximation( ResultSet $firstPassResults ) {
+		if ( !$this->query->isAllowRewrite() ) {
+			return 0.0;
+		}
+		if ( $firstPassResults->numRows() > 0 ) {
+			return 0.0;
+		}
+		if ( !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
+			return 0.0;
+		}
+		if ( $firstPassResults->hasSuggestion() ) {
+			return 0.5;
+		}
+		return 0.0;
+	}
+
+	/**
+	 * @param ResultSet $firstPassResults
+	 * @param ResultSet $previousSet
+	 * @return ResultSet
+	 */
+	public function rewrite( ResultSet $firstPassResults, ResultSet $previousSet ) {
+		if ( $previousSet->numRows() > 0 ) {
+			return $previousSet;
+		}
+		$rewrittenQuery = SearchQueryBuilder::forRewrittenQuery( $this->query,
+			$firstPassResults->getSuggestionQuery() )->build();
+		$searcher = $this->searcherFactory->makeSearcher( $rewrittenQuery );
+		$status = $searcher->search( $rewrittenQuery );
+		if ( $status->isOK() && $status->getValue() instanceof ResultSet ) {
+			/**
+			 * @var ResultSet $newresults
+			 */
+			$newresults = $status->getValue();
+			$newresults->setRewrittenQuery( $firstPassResults->getSuggestionQuery(),
+				$firstPassResults->getSuggestionSnippet() );
+			return $newresults;
+		} else {
+			return $previousSet;
+		}
+	}
+}

includes/Search/ResultSet.php
@@ -73,6 +73,7 @@ class ResultSet extends SearchResultSet {
 		$this->totalHits = $res->getTotalHits();
 		$this->preCacheContainedTitles( $this->result );
 		$suggestion = $this->findSuggestion();
+		// TODO: move all the DYM "suggestion" code to PhraseSuggestFallbackMethod (once we get rid of suggest[Prefixes/Suffixes])
 		if ( $suggestion && ! $this->resultContainsFullyHighlightedMatch() ) {
 			$this->suggestionQuery = $suggestion[ 'text' ];
 			$this->suggestionSnippet = $this->escapeHighlightedSuggestion( $suggestion[ 'highlighted' ] );

tests/phpunit/DummyResultSet.php
@@ -1,0 +1,77 @@
+<?php
+
+namespace CirrusSearch;
+
+use CirrusSearch\Search\ResultSet;
+
+class DummyResultSet extends ResultSet {
+	/**
+	 * @var string|null
+	 */
+	private $overriddenSuggestion = null;
+
+	/**
+	 * @var string|null
+	 */
+	private $overriddenSuggestionSnippet;
+
+	/**
+	 * @var bool
+	 */
+	private $useOverriddenSuggestion = false;
+
+	/**
+	 * DummyResultSet constructor.
+	 * @param array $results
+	 * @param bool $withSyntax
+	 */
+	public function __construct( array $results, $withSyntax = false ) {
+		parent::__construct( [], [],
+			new \Elastica\ResultSet( new \Elastica\Response( '{}' ),
+				new \Elastica\Query(),
+				[] ),
+			$withSyntax );
+		$this->results = $results;
+	}
+
+	/**
+	 * @param int $numRows
+	 * @return DummyResultSet
+	 */
+	public static function fakeNumRows( $numRows ) {
+		return new self( array_fill( 0, $numRows, null ) );
+	}
+
+	/**
+	 * @param int $numRows
+	 * @param string|null $suggestionQuery
+	 * @param null $suggestionSnippet
+	 * @return DummyResultSet
+	 */
+	public static function fakeNumRowWithSuggestion( $numRows, $suggestionQuery = null, $suggestionSnippet = null ) {
+		$res = self::fakeNumRows( $numRows );
+		$res->overriddenSuggestion = $suggestionQuery;
+		$res->overriddenSuggestionSnippet = $suggestionSnippet;
+		$res->useOverriddenSuggestion = true;
+
+		return $res;
+	}
+
+	/**
+	 * @return null|string
+	 */
+	public function getSuggestionQuery() {
+		return $this->useOverriddenSuggestion ? $this->overriddenSuggestion : parent::getSuggestionQuery();
+	}
+
+	public function getSuggestionSnippet() {
+		return $this->useOverriddenSuggestion ? $this->overriddenSuggestionSnippet : parent::getSuggestionSnippet();
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function hasSuggestion() {
+		return $this->useOverriddenSuggestion ? $this->overriddenSuggestion != null : parent::hasSuggestion();
+	}
+}

commit 00642dce7d61cce171dd48709a8a9488f8c5f262

Language detection should not be run when rewriting is not enabled
regression introduced in 4ca4565 where the rewrite flag stopped to be
checked.
Now use getTotalHits() instead of numRows() to check the result
threshold. Some clients may ask a single result thus reaching the
threshold for all their queries even if cirrus could have returned
millions of results.

Bug: T212455
Change-Id: Ib829d35e7d892cf0f40ee24fd7c0607dd6b01418

autoload.php
@@ -61,6 +61,7 @@ $wgAutoloadClasses += [
 	'CirrusSearch\\Extra\\Query\\TokenCountRouter' => __DIR__ . '/includes/Extra/Query/TokenCountRouter.php',
 	'CirrusSearch\\Fallbacks\\BaseFallbackMethodTest' => __DIR__ . '/tests/phpunit/Fallbacks/BaseFallbackMethodTest.php',
 	'CirrusSearch\\Fallbacks\\FallbackMethod' => __DIR__ . '/includes/Fallbacks/FallbackMethod.php',
+	'CirrusSearch\\Fallbacks\\FallbackMethodTrait' => __DIR__ . '/includes/Fallbacks/FallbackMethodTrait.php',
 	'CirrusSearch\\Fallbacks\\FallbackRunner' => __DIR__ . '/includes/Fallbacks/FallbackRunner.php',
 	'CirrusSearch\\Fallbacks\\LangDetectFallbackMethod' => __DIR__ . '/includes/Fallbacks/LangDetectFallbackMethod.php',
 	'CirrusSearch\\Fallbacks\\PhraseSuggestFallbackMethod' => __DIR__ . '/includes/Fallbacks/PhraseSuggestFallbackMethod.php',

includes/Fallbacks/FallbackMethodTrait.php
@@ -1,0 +1,32 @@
+<?php
+
+namespace CirrusSearch\Fallbacks;
+
+use CirrusSearch\Search\ResultSet;
+
+trait FallbackMethodTrait {
+
+	/**
+	 * Check the number of total hits stored in $resultSet
+	 * and return true if it's greater or equals than $threshold
+	 * NOTE: inter wiki results are check
+	 *
+	 * @param ResultSet $resultSet
+	 * @param int $threshold (defaults to 1).
+	 *
+	 * @see \SearchResultSet::getInterwikiResults()
+	 * @see \SearchResultSet::SECONDARY_RESULTS
+	 * @return bool
+	 */
+	public function resultsThreshold( ResultSet $resultSet, $threshold = 1 ) {
+		if ( $resultSet->getTotalHits() >= $threshold ) {
+			return true;
+		}
+		foreach ( $resultSet->getInterwikiResults( \SearchResultSet::SECONDARY_RESULTS ) as $resultSet ) {
+			if ( $resultSet->getTotalHits() >= $threshold ) {
+				return true;
+			}
+		}
+		return false;
+	}
+}

includes/Fallbacks/LangDetectFallbackMethod.php
@@ -16,6 +16,7 @@ use MediaWiki\MediaWikiServices;
 use Wikimedia\Assert\Assert;
 
 class LangDetectFallbackMethod implements FallbackMethod, SearchMetricsProvider {
+	use FallbackMethodTrait;
 
 	/**
 	 * @var SearchQuery
@@ -90,10 +91,13 @@ class LangDetectFallbackMethod implements FallbackMethod, SearchMetricsProvider 
 	 * @return float
 	 */
 	public function successApproximation( ResultSet $firstPassResults ) {
+		if ( !$this->query->isAllowRewrite() ) {
+			return 0.0;
+		}
 		if ( !$this->query->getCrossSearchStrategy()->isCrossLanguageSearchSupported() ) {
 			return 0.0;
 		}
-		if ( $firstPassResults->numRows() >= $this->threshold ) {
+		if ( $this->resultsThreshold( $firstPassResults, $this->threshold ) ) {
 			return 0.0;
 		}
 		if ( !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
@@ -141,8 +145,7 @@ class LangDetectFallbackMethod implements FallbackMethod, SearchMetricsProvider 
 		Assert::precondition( $this->detectedLangWikiConfig !== null,
 			'nothing has been detected, this should not even be tried.' );
 
-		if ( $previousSet->numRows() >= $this->threshold ||
-			 !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
+		if ( $this->resultsThreshold( $previousSet, $this->threshold ) ) {
 			return $previousSet;
 		}
 

includes/Fallbacks/PhraseSuggestFallbackMethod.php
@@ -13,6 +13,7 @@ use CirrusSearch\Search\SearchQueryBuilder;
  * but in the future it'll be responsible from setting the suggestion in the ResultSet
  */
 class PhraseSuggestFallbackMethod implements FallbackMethod {
+	use FallbackMethodTrait;
 
 	/**
 	 * @var SearchQuery
@@ -52,9 +53,11 @@ class PhraseSuggestFallbackMethod implements FallbackMethod {
 		if ( !$this->query->isAllowRewrite() ) {
 			return 0.0;
 		}
-		if ( $firstPassResults->numRows() > 0 ) {
+
+		if ( $this->resultsThreshold( $firstPassResults ) ) {
 			return 0.0;
 		}
+
 		if ( !$this->query->getParsedQuery()->isQueryOfClass( BasicQueryClassifier::SIMPLE_BAG_OF_WORDS ) ) {
 			return 0.0;
 		}
@@ -70,9 +73,10 @@ class PhraseSuggestFallbackMethod implements FallbackMethod {
 	 * @return ResultSet
 	 */
 	public function rewrite( ResultSet $firstPassResults, ResultSet $previousSet ) {
-		if ( $previousSet->numRows() > 0 ) {
+		if ( $this->resultsThreshold( $previousSet ) ) {
 			return $previousSet;
 		}
+
 		$rewrittenQuery = SearchQueryBuilder::forRewrittenQuery( $this->query,
 			$firstPassResults->getSuggestionQuery() )->build();
 		$searcher = $this->searcherFactory->makeSearcher( $rewrittenQuery );

includes/Search/EmptyResultSet.php
@@ -37,16 +37,6 @@ class EmptyResultSet extends ResultSet {
 	}
 
 	/**
-	 * Is rewriting this query OK?
-	 *
-	 * @param int $threshold Minimum number of results to reach before rewriting is not allowed.
-	 * @return bool True when rewriting this query is allowed
-	 */
-	public function isQueryRewriteAllowed( $threshold = 1 ) {
-		return false;
-	}
-
-	/**
 	 * @return int
 	 */
 	public function getTotalHits() {

includes/Search/ResultSet.php
@@ -113,21 +113,13 @@ class ResultSet extends SearchResultSet {
 	}
 
 	/**
-	 * Is rewriting this query OK?
-	 *
-	 * @param int $threshold Minimum number of results to reach before rewriting is not allowed.
-	 * @return bool True when rewriting this query is allowed
+	 * @param int $threshold
+	 * @return bool always false
+	 * @deprecated always return false, moved to \CirrusSearch\Fallbacks\FallbackMethodTrait::resultsThreshold()
+	 * @see \CirrusSearch\Fallbacks\FallbackMethodTrait::resultsThreshold()
 	 */
 	public function isQueryRewriteAllowed( $threshold = 1 ) {
-		if ( $this->numRows() >= $threshold || $this->searchContainedSyntax() ) {
-			return false;
-		}
-		foreach ( $this->getInterwikiResults( SearchResultSet::SECONDARY_RESULTS ) as $resultSet ) {
-			if ( $resultSet->numRows() >= $threshold ) {
-				return false;
-			}
-		}
-		return true;
+		return false;
 	}
 
 	/**

tests/phpunit/DummyResultSet.php
@@ -3,6 +3,7 @@
 namespace CirrusSearch;
 
 use CirrusSearch\Search\ResultSet;
+use SearchResultSet;
 
 class DummyResultSet extends ResultSet {
 	/**
@@ -22,34 +23,39 @@ class DummyResultSet extends ResultSet {
 
 	/**
 	 * DummyResultSet constructor.
-	 * @param array $results
+	 * @param int $totalHits
 	 * @param bool $withSyntax
 	 */
-	public function __construct( array $results, $withSyntax = false ) {
+	public function __construct( $totalHits, $withSyntax = false ) {
 		parent::__construct( [], [],
-			new \Elastica\ResultSet( new \Elastica\Response( '{}' ),
+			new \Elastica\ResultSet( new \Elastica\Response( [ "hits" => [ "total" => $totalHits ] ] ),
 				new \Elastica\Query(),
 				[] ),
 			$withSyntax );
-		$this->results = $results;
+		$this->results = array_fill( 0, min( $totalHits, 20 ), null );
 	}
 
 	/**
-	 * @param int $numRows
+	 * @param int $totalHits
+	 * @param int[] $interwikiTotals total hits for secondary results interwiki results.
 	 * @return DummyResultSet
 	 */
-	public static function fakeNumRows( $numRows ) {
-		return new self( array_fill( 0, $numRows, null ) );
+	public static function fakeTotalHits( $totalHits, array $interwikiTotals = [] ) {
+		$results = new self( $totalHits );
+		foreach ( $interwikiTotals as $pref => $iwTotal ) {
+			$results->addInterwikiResults( self::fakeTotalHits( $iwTotal ), SearchResultSet::SECONDARY_RESULTS, (string)$pref );
+		}
+		return $results;
 	}
 
 	/**
-	 * @param int $numRows
+	 * @param int $totalHits
 	 * @param string|null $suggestionQuery
 	 * @param null $suggestionSnippet
 	 * @return DummyResultSet
 	 */
-	public static function fakeNumRowWithSuggestion( $numRows, $suggestionQuery = null, $suggestionSnippet = null ) {
-		$res = self::fakeNumRows( $numRows );
+	public static function fakeTotalHitsWithSuggestion( $totalHits, $suggestionQuery = null, $suggestionSnippet = null ) {
+		$res = self::fakeTotalHits( $totalHits );
 		$res->overriddenSuggestion = $suggestionQuery;
 		$res->overriddenSuggestionSnippet = $suggestionSnippet;
 		$res->useOverriddenSuggestion = true;

tests/phpunit/Fallbacks/BaseFallbackMethodTest.php
@@ -3,6 +3,7 @@
 namespace CirrusSearch\Fallbacks;
 
 use CirrusSearch\CirrusTestCase;
+use CirrusSearch\DummyResultSet;
 use CirrusSearch\Search\ResultSet;
 use CirrusSearch\Search\SearchQuery;
 use CirrusSearch\Searcher;
@@ -24,5 +25,85 @@ class BaseFallbackMethodTest extends CirrusTestCase {
 			->method( 'makeSearcher' )
 			->willReturn( $searcherMock );
 		return $mock;
+	}
+
+	public function provideTestResultThreshold() {
+		return [
+			'threshold is not reached' => [
+				1,
+				0,
+				[],
+				false
+			],
+			'threshold is not reached even with interwiki results' => [
+				1,
+				0,
+				[ 0, 0 ],
+				false,
+			],
+			'threshold is reached' => [
+				1,
+				1,
+				[],
+				true
+			],
+			'threshold is reached reading interwiki results' => [
+				1,
+				0,
+				[ 0, 1, 0 ],
+				true
+			],
+			'threshold can be greater than 1 and not reached' => [
+				3,
+				2,
+				[],
+				false
+			],
+			'threshold can be greater than 1 and not reached with interwiki results' => [
+				3,
+				0,
+				[ 0, 2, 0 ],
+				false,
+			],
+			'threshold can be greater than 1 and reached' => [
+				3,
+				3,
+				[],
+				true
+			],
+			'threshold can be greater than 1 and reached with interwiki results' => [
+				3,
+				0,
+				[ 0, 3, 0 ],
+				true,
+			],
+			'threshold can be greater than 1 and exceeded' => [
+				3,
+				5,
+				[],
+				true
+			],
+			'threshold can be greater than 1 and exceeded with interwiki results' => [
+				3,
+				0,
+				[ 0, 5, 0 ],
+				true,
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideTestResultThreshold
+	 * @covers \CirrusSearch\Fallbacks\FallbackMethodTrait::resultsThreshold()
+	 */
+	public function testResultThreshold( $threshold, $mainTotal, array $interwikiTotals, $met ) {
+		$resultSet = DummyResultSet::fakeTotalHits( $mainTotal, $interwikiTotals );
+		$mock = $this->getMockForTrait( FallbackMethodTrait::class );
+		$this->assertEquals( $met, $mock->resultsThreshold( $resultSet, $threshold ) );
+		if ( $threshold === 1 ) {
+			// Test default method param
+			$this->assertEquals( $mock->resultsThreshold( $resultSet ),
+				$mock->resultsThreshold( $resultSet, $threshold ) );
+		}
 	}
 }

tests/phpunit/Fallbacks/FallbackRunnerTest.php
@@ -26,7 +26,7 @@ class FallbackRunnerTest extends CirrusTestCase {
 	private $execOrder = [];
 
 	public function testOrdering() {
-		$results = DummyResultSet::fakeNumRows( 0 );
+		$results = DummyResultSet::fakeTotalHits( 0 );
 		$methods = [];
 
 		$methods[] = $this->getFallbackMethod( 0.1, $this->trackingCb( 'E' ), [ 'E' => 'E' ] );
@@ -48,7 +48,7 @@ class FallbackRunnerTest extends CirrusTestCase {
 	}
 
 	public function testEarlyStop() {
-		$results = DummyResultSet::fakeNumRows( 0 );
+		$results = DummyResultSet::fakeTotalHits( 0 );
 		$methods = [];
 
 		$methods[] = self::getFallbackMethod( 0.1 );
@@ -164,17 +164,17 @@ class FallbackRunnerTest extends CirrusTestCase {
 		$searcherFactory->expects( $this->exactly( 2 ) )
 			->method( 'makeSearcher' )
 			->willReturnOnConsecutiveCalls(
-				$this->mockSearcher( DummyResultSet::fakeNumRows( 2 ) ),
-				$this->mockSearcher( DummyResultSet::fakeNumRows( 3 ) )
+				$this->mockSearcher( DummyResultSet::fakeTotalHits( 2 ) ),
+				$this->mockSearcher( DummyResultSet::fakeTotalHits( 3 ) )
 			);
 		$runner = FallbackRunner::create( $searcherFactory, $query, $request );
-		$initialResults = DummyResultSet::fakeNumRowWithSuggestion( 0, 'foobar' );
+		$initialResults = DummyResultSet::fakeTotalHitsWithSuggestion( 0, 'foobar' );
 		$newResults = $runner->run( $initialResults );
-		$this->assertEquals( 2, $newResults->numRows() );
+		$this->assertEquals( 2, $newResults->getTotalHits() );
 		$iwResults = $newResults->getInterwikiResults( \SearchResultSet::INLINE_RESULTS );
 		$this->assertNotEmpty( $iwResults );
 		$this->assertArrayHasKey( 'frwiki', $iwResults );
-		$this->assertEquals( 3, $iwResults['frwiki']->numRows() );
+		$this->assertEquals( 3, $iwResults['frwiki']->getTotalHits() );
 		$this->assertArrayEquals(
 			[
 				'wgCirrusSearchAltLanguageNumResults' => 3,

tests/phpunit/Fallbacks/LangDetectFallbackMethodTest.php
@@ -131,7 +131,7 @@ class LangDetectFallbackMethodTest extends BaseFallbackMethodTest {
 		$query = SearchQueryBuilder::newFTSearchQueryBuilder( $config, $query )
 			->setAllowRewrite( true )
 			->build();
-		$expectedRewrittenResults = $secondTryNumResults >= 0 ? DummyResultSet::fakeNumRows( $secondTryNumResults ) : null;
+		$expectedRewrittenResults = $secondTryNumResults >= 0 ? DummyResultSet::fakeTotalHits( $secondTryNumResults ) : null;
 		$searcherFactory = null;
 		if ( $expectedScoreApprox > 0 ) {
 			$searcherFactory = $this->getSearcherFactoryMock(
@@ -149,7 +149,7 @@ class LangDetectFallbackMethodTest extends BaseFallbackMethodTest {
 			],
 			$this->getInterwikiMock( $targetWikiConfig, $returnedLang !== 'en' ? $returnedLang : null ) );
 
-		$initialResults = DummyResultSet::fakeNumRows( $initialNumResults );
+		$initialResults = DummyResultSet::fakeTotalHits( $initialNumResults );
 		$this->assertEquals( $expectedScoreApprox, $fallback->successApproximation( $initialResults ) );
 		if ( $expectedScoreApprox > 0 ) {
 			$rewrittenResults = $fallback->rewrite( $initialResults, $initialResults );
@@ -164,6 +164,41 @@ class LangDetectFallbackMethodTest extends BaseFallbackMethodTest {
 			}
 		}
 		$this->assertEquals( $expectedMetrics, $fallback->getMetrics() );
+	}
+
+	public function provideTestNotRunWhenRewriteDisabled() {
+		return [
+			'allowed' => [ true, 0.5 ],
+			'not allowed' => [ false, 0.0 ],
+		];
+	}
+
+	/**
+	 * @dataProvider provideTestNotRunWhenRewriteDisabled()
+	 */
+	public function testNotRunWhenRewriteDisabled( $allowRewrite, $expectedScore ) {
+		$config = new HashSearchConfig( [
+			'CirrusSearchInterwikiThreshold' => 2,
+			'CirrusSearchEnableAltLanguage' => true,
+			'LanguageCode' => 'en',
+		] );
+		$targetWikiConfig = new HashSearchConfig( [
+			'_wikiID' => 'targetwiki',
+			'LanguageCode' => 'fr',
+		] );
+
+		$query =
+			SearchQueryBuilder::newFTSearchQueryBuilder( $config, 'foo' )
+				->setAllowRewrite( $allowRewrite )
+				->build();
+
+		$searcherFactory = $this->getSearcherFactoryMock( null, null );
+		$fallback = new LangDetectFallbackMethod( $query, $searcherFactory,
+			[ 'tested_detector' => $this->getLanguageDetector( 'fr' ) ],
+			$this->getInterwikiMock( $targetWikiConfig, $allowRewrite ? 'fr' : null ) );
+
+		$initialResults = DummyResultSet::fakeTotalHits( 0 );
+		$this->assertEquals( $expectedScore, $fallback->successApproximation( $initialResults ) );
 	}
 
 	public function getLanguageDetector( $expectedLang ) {

tests/phpunit/Fallbacks/PhraseSuggestFallbackMethodTest.php
@@ -61,10 +61,11 @@ class PhraseSuggestFallbackMethodTest extends BaseFallbackMethodTest {
 			->build();
 		$rewritten = $suggestionQuery !== null ? SearchQueryBuilder::forRewrittenQuery( $query, $suggestionQuery )->build() : null;
 
-		$rewrittenResults = $numRewrittenResults >= 0 ? DummyResultSet::fakeNumRows( $numRewrittenResults ) : null;
+		$rewrittenResults = $numRewrittenResults >= 0 ? DummyResultSet::fakeTotalHits( $numRewrittenResults ) : null;
 		$searcherFactory = $this->getSearcherFactoryMock( $expectedApproxScore > 0 ? $rewritten : null, $rewrittenResults );
 		$fallback = new PhraseSuggestFallbackMethod( $searcherFactory, $query );
-		$initialResults = DummyResultSet::fakeNumRowWithSuggestion( $numInitialResults, $suggestionQuery, htmlspecialchars( $suggestionQuery ) );
+		$initialResults = DummyResultSet::fakeTotalHitsWithSuggestion( $numInitialResults,
+			$suggestionQuery, htmlspecialchars( $suggestionQuery ) );
 		$this->assertEquals( $expectedApproxScore, $fallback->successApproximation( $initialResults ) );
 		if ( $expectedApproxScore > 0 ) {
 			$actualNewResults = $fallback->rewrite( $initialResults, $initialResults );
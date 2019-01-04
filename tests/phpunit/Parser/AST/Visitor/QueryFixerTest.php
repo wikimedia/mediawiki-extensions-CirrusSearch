a10890f61e86b9ebde933a8ba2342205944217b8
David Causse <dcausse@wikimedia.org>
2019-01-04 22:04:43 +0100

    Add QueryFixer
    Is a simple visitor that tries to detect in a query what can be fixed
    and the additional fix method to repair a query using an external
    corrector (e.g. typo).
    This class is meant to unify some of the logic around the phrase suggester.
    It was designed to mimic as much as possible the current behavior excluding
    incorrect incoherent ones.
    We may not want to stick with these behaviors as I'm pretty sure most of them
    were side-effect of the current parsing technique.
    Dropped the support of merging two bags of words that are separated with a
    keyword, I'm not sure it's right to consider them a "phrase". With a smarter
    QueryFixer we could re-add this but not sure it's worthwhile.
    
    Bug: T213098
    Change-Id: Iabb63e794044378e80f12758c5692cf1fed324fe

---
+++ tests/phpunit/Parser/AST/Visitor/QueryFixerTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace CirrusSearch\Parser\AST\Visitor;
+
+use CirrusSearch\CirrusTestCase;
+use CirrusSearch\HashSearchConfig;
+use CirrusSearch\Parser\QueryParserFactory;
+
+/**
+ * @covers \CirrusSearch\Parser\AST\Visitor\QueryFixer
+ */
+class QueryFixerTest extends CirrusTestCase {
+
+	public function provideTest() {
+		return [
+			'simple words' => [
+				'hello wolrd',
+				'hello wolrd',
+				'hello world',
+				'hello world'
+			],
+			'sometimes nothing is extracted' => [
+				'filetype:foo',
+				null,
+				null,
+				null,
+			],
+			'longest is fixed' => [
+				'foo hastemplate:bar hello wolrd',
+				"hello wolrd",
+				"hello world",
+				'foo hastemplate:bar hello world'
+			],
+			'complex query with negated parts are not fixed' => [
+				'fou "foo" NOT helloworld',
+				null,
+				null,
+				null,
+			],
+			'dash negation inhibits the fixer' => [
+				'hellow -wolrd',
+				null,
+				null,
+				null,
+			],
+			'dash negation in front of keywords is OK' => [
+				'-hastemplate:test hello wolrd',
+				"hello wolrd",
+				"hello world",
+				"-hastemplate:test hello world",
+			],
+			'intitle is fixed (perhaps side-effect of legacy code)' => [
+				'foo hastemplate:sep intitle:wolrd',
+				"wolrd",
+				"world",
+				"foo hastemplate:sep intitle:world",
+			],
+			'inhibits the fixer on unacceptable in chars in keywords (legacy browsertest_176)' => [
+				'intitle:hell\\? intitle:wolrd',
+				null,
+				null,
+				null,
+			],
+			'words that requires escaping are ignored' => [
+				'\\"hello hastemplate:sep \\\\wolrd wolr\\*d hastemplate:sep wolr\\\\?d',
+				null,
+				null,
+				null
+			],
+			'tilde prefix is kept' => [
+				'~hello wolrd',
+				'hello wolrd',
+				'hello world',
+				'~hello world'
+			],
+			'ns prefix is kept' => [
+				'file:hello wolrd',
+				'hello wolrd',
+				'hello world',
+				'file:hello world'
+			],
+			'tilde & ns prefixes are kept' => [
+				'~file:hello wolrd',
+				'hello wolrd',
+				'hello world',
+				'~file:hello world'
+			],
+			'stripped qmark are left behind' => [
+				'hello wolrd?',
+				'hello wolrd',
+				'hello world',
+				'hello world '
+			],
+			'escaped qmark inhibits query fixer' => [
+				'hello wolrd\\?',
+				null,
+				null,
+				null,
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider provideTest
+	 */
+	public function test( $input, $extracted, $fixedString, $expectedFixedQuery ) {
+		$parser = QueryParserFactory::newFullTextQueryParser( new HashSearchConfig( [ 'CirrusSearchStripQuestionMarks' => 'all' ] ) );
+		$parsed = $parser->parse( $input );
+		$fixer = new QueryFixer( $parsed );
+		$this->assertEquals( $extracted, $fixer->getFixablePart() );
+		$this->assertEquals( $expectedFixedQuery, $fixer->fix( $fixedString ) );
+	}
+
+	public function testEscape() {
+		$parser = QueryParserFactory::newFullTextQueryParser( new HashSearchConfig( [ 'CirrusSearchStripQuestionMarks' => 'all' ] ) );
+		$parsed = $parser->parse( 'intitle:< hello world intitle:>' );
+		$fixer = new QueryFixer( $parsed );
+		$this->assertNotNull( $fixer->getFixablePart() );
+		$this->assertEquals( 'intitle:&lt; hello <em>world</em> intitle:&gt;', $fixer->fix( 'hello <em>world</em>', true ) );
+	}
+}

324efb1455771e6fd1b68faeeacb86357c18e784
Erik Bernhardson <ebernhardson@wikimedia.org>
2019-01-24 12:49:35 -0800

    Remove max_determined_states BC
    As we transition to elasticsearc 6 this BC is no longer necessary.
    
    Change-Id: I9a58784ac179a73e1f18e9272e8633b8095a4354

--- includes/Query/FullTextQueryStringQueryBuilder.php
+++ includes/Query/FullTextQueryStringQueryBuilder.php
@@ -321,15 +321,8 @@ class FullTextQueryStringQueryBuilder implements FullTextQueryBuilder {
 		$query->setFuzzyPrefixLength( 2 );
 		$query->setRewrite( $this->getMultiTermRewriteMethod() );
 		$states = $this->config->get( 'CirrusSearchQueryStringMaxDeterminizedStates' );
-		$option = 'max_determinized_states';
-		// Workround https://github.com/elastic/elasticsearch/issues/22722
-		if ( $this->config->getElement(
-			'CirrusSearchElasticQuirks', 'query_string_max_determinized_states' ) === true
-		) {
-			$option = 'max_determined_states';
-		}
 		if ( isset( $states ) ) {
-			$query->setParam( $option, $states );
+			$query->setParam( 'max_determinized_states', $states );
 		}
 		return $query;
 	}

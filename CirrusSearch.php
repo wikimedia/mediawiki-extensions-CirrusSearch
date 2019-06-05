c618982e13712bb7e208691ba8ea998c5a67d906
Reedy <reedy@wikimedia.org>
2019-06-05 14:04:40 +0000

    Re-add $wgMessagesDirs to CirrusSearch.php
    Change-Id: Idd4f8e501331f400fdf0c19e5079eb3b18a5e57f

--- CirrusSearch.php
+++ CirrusSearch.php
@@ -2,6 +2,11 @@
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'CirrusSearch' );
+	$wgMessagesDirs['CirrusSearch'] = [
+		__DIR__ . '/i18n',
+		__DIR__ . '/i18n/api',
+	];
+
 /* Warning disabled for config migration
 	wfWarn( 'Deprecated PHP entry point used for CirrusSearch extension. Please use wfLoadExtension ' .
 		'instead, see https://www.mediawiki.org/wiki/Extension_registration for more details.'

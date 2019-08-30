1ae0785fe509a8597328d11423d0c346947a3cdf
David Causse <dcausse@wikimedia.org>
2019-08-30 15:58:19 +0200

    Add morelikethis a non-greedy version of the morelike keyword
    morelike is a keyword that cannot be combined because it is greedy
    and considered a query header. This does not allow to combine it
    with any other keywords/query text.
    Since morelike is heavily used it is not possible to fix without
    breaking existing clients.
    Introduce a new keyword "morelikethis" to address all the limitations
    of morelike.
    Like any other keywords its value must be wrapped in double quotes if
    it contains space.
    Internally it will act as a more_like feature:
    - be reported as such in metrics
    - cached using the same TTL config
    
    Bug: T159321
    Change-Id: I81bda72fa7fb4f544d4bed701d441a473d00329e
    (cherry picked from commit cd8ef9520c06c0ec396bd18c1cf4efa189869972)

--- includes/Query/MoreLikeFeature.php
+++ includes/Query/MoreLikeFeature.php
@@ -8,9 +8,13 @@ use CirrusSearch\Search\SearchContext;
 use CirrusSearch\SearchConfig;
 use CirrusSearch\WarningCollector;
 use Title;
-use WikiPage;
 
+/**
+ * Finds pages similar to another one.
+ * Greedy keyword kept for BC purposes, MoreLikeThisFeature should be preferred.
+ */
 class MoreLikeFeature extends SimpleKeywordFeature implements LegacyKeywordFeature {
+	use MoreLikeTrait;
 	const MORE_LIKE_THIS = 'morelike';
 
 	/**
@@ -26,7 +30,6 @@ class MoreLikeFeature extends SimpleKeywordFeature implements LegacyKeywordFeatu
 	}
 
 	/**
-	 * TODO: switch to non-greedy
 	 * @return bool
 	 */
 	public function greedy() {
@@ -35,7 +38,6 @@ class MoreLikeFeature extends SimpleKeywordFeature implements LegacyKeywordFeatu
 
 	/**
 	 * morelike is only allowed at the beginning of the query
-	 * TODO: allow morelike everywhere
 	 * @return bool
 	 */
 	public function queryHeader() {
@@ -81,8 +83,8 @@ class MoreLikeFeature extends SimpleKeywordFeature implements LegacyKeywordFeatu
 		}
 		$query = $this->buildMoreLikeQuery( $titles );
 
-		// FIXME: this erases the main query making it impossible to combine with
-		// other keywords/search query
+		// this erases the main query making it impossible to combine with
+		// other keywords/search query. MoreLikeThisFeature addresses this problem.
 		$context->setMainQuery( $query );
 
 		// highlight snippets are not great so it's worth running a match all query
@@ -103,124 +105,9 @@ class MoreLikeFeature extends SimpleKeywordFeature implements LegacyKeywordFeatu
 	}
 
 	/**
-	 * @param string $key
-	 * @param string $term
-	 * @param WarningCollector $warningCollector
-	 * @return Title[]
+	 * @return SearchConfig
 	 */
-	private function doExpand( $key, $term, WarningCollector $warningCollector ) {
-		// If no fields have been set we return no results. This can happen if
-		// the user override this setting with field names that are not allowed
-		// in $this->config->get( 'CirrusSearchMoreLikeThisAllowedFields' )
-		// (see Hooks.php)
-		if ( !$this->config->get( 'CirrusSearchMoreLikeThisFields' ) ) {
-			$warningCollector->addWarning( "cirrussearch-mlt-not-configured",  $key );
-			return [];
-		}
-		$titles = $this->collectTitles( $term );
-		if ( $titles === [] ) {
-			$warningCollector->addWarning( "cirrussearch-mlt-feature-no-valid-titles", $key );
-		}
-		return $titles;
-	}
-
-	/**
-	 * @param string $term
-	 * @return Title[]
-	 */
-	private function collectTitles( $term ) {
-		if ( $this->config->getElement( 'CirrusSearchDevelOptions',
-			'morelike_collect_titles_from_elastic' )
-		) {
-			return $this->collectTitlesFromElastic( $term );
-		} else {
-			return $this->collectTitlesFromDB( $term );
-		}
-	}
-
-	/**
-	 * Use for devel purpose only
-	 * @param string $terms
-	 * @return Title[]
-	 */
-	private function collectTitlesFromElastic( $terms ) {
-		$titles = [];
-		foreach ( explode( '|', $terms ) as $term ) {
-			$title = null;
-			\CirrusSearch\Hooks::onSearchGetNearMatch( $term, $title );
-			if ( $title != null ) {
-				$titles[] = $title;
-			}
-		}
-		return $titles;
-	}
-
-	/**
-	 * @param string $term
-	 * @return Title[]
-	 */
-	private function collectTitlesFromDB( $term ) {
-		$titles = [];
-		$found = [];
-		foreach ( explode( '|', $term ) as $title ) {
-			$title = Title::newFromText( trim( $title ) );
-			while ( true ) {
-				if ( !$title ) {
-					continue 2;
-				}
-				$titleText = $title->getFullText();
-				if ( isset( $found[$titleText] ) ) {
-					continue 2;
-				}
-				$found[$titleText] = true;
-				if ( !$title->exists() ) {
-					continue 2;
-				}
-				if ( !$title->isRedirect() ) {
-					break;
-				}
-				// If the page was a redirect loop the while( true ) again.
-				$page = WikiPage::factory( $title );
-				if ( !$page->exists() ) {
-					continue 2;
-				}
-				$title = $page->getRedirectTarget();
-			}
-			$titles[] = $title;
-		}
-
-		return $titles;
-	}
-
-	/**
-	 * Builds a more like this query for the specified titles. Take care that
-	 * this outputs a stable result, regardless of order of configuration
-	 * parameters and input titles. The result of this is hashed to generate an
-	 * application side cache key. If the result is unstable we will see a
-	 * reduced hit rate, and waste cache storage space.
-	 *
-	 * @param Title[] $titles
-	 * @return \Elastica\Query\MoreLikeThis
-	 */
-	private function buildMoreLikeQuery( array $titles ) {
-		sort( $titles, SORT_STRING );
-		$docIds = [];
-		$likeDocs = [];
-		foreach ( $titles as $title ) {
-			$docId = $this->config->makeId( $title->getArticleID() );
-			$docIds[] = $docId;
-			$likeDocs[] = [ '_id' => $docId ];
-		}
-
-		$moreLikeThisFields = $this->config->get( 'CirrusSearchMoreLikeThisFields' );
-		sort( $moreLikeThisFields );
-		$query = new \Elastica\Query\MoreLikeThis();
-		$query->setParams( $this->config->get( 'CirrusSearchMoreLikeThisConfig' ) );
-		$query->setFields( $moreLikeThisFields );
-
-		/** @phan-suppress-next-line PhanTypeMismatchArgument library is mis-annotated */
-		$query->setLike( $likeDocs );
-
-		return $query;
+	public function getConfig(): SearchConfig {
+		return $this->config;
 	}
 }

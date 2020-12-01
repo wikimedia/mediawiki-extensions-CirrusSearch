9592fb0afc10a7bfbe2c3440a058d5e0d0aa57b7
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2020-12-01 08:35:25 +0100

    Wrap `new mw.Uri()` constructors in try-catch
    These can fail for several reasons, e.g. when there is a
    parameter in the URL that can't be URL-decoded because of
    a bad %-sequence.
    
    Bug: T268067
    Change-Id: I9378600be5a6c15b7a81457d5d4dec6caa396cba

--- resources/ext.cirrus.serp.js
+++ resources/ext.cirrus.serp.js
@@ -1,19 +1,30 @@
 $( function () {
-	var uri = new mw.Uri( location.href );
 	if ( !history.replaceState ) {
 		return;
 	}
-	if ( uri.query.searchToken ) {
-		delete uri.query.searchToken;
-		history.replaceState( {}, '', uri.toString() );
+
+	try {
+		var uri = new mw.Uri( location.href );
+		if ( uri.query.searchToken ) {
+			delete uri.query.searchToken;
+			history.replaceState( {}, '', uri.toString() );
+		}
+	} catch ( e ) {
+		// Don't install the click handler when the browser location can't be parsed anyway
+		return;
+	}
+
+	// No need to install the click handler when there is no token
+	if ( !mw.config.get( 'wgCirrusSearchRequestSetToken' ) ) {
+		return;
 	}
 
 	$( document ).on( 'click', 'a', function () {
-		var clickUri = new mw.Uri( location.href ),
-			token = mw.config.get( 'wgCirrusSearchRequestSetToken' );
-		if ( token ) {
-			clickUri.query.searchToken = token;
+		try {
+			var clickUri = new mw.Uri( location.href );
+			clickUri.query.searchToken = mw.config.get( 'wgCirrusSearchRequestSetToken' );
 			history.replaceState( {}, '', clickUri.toString() );
+		} catch ( e ) {
 		}
 	} );
 } );

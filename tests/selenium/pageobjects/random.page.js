c470e4bdb697061fe1f9ca82f15c16b13f4d0e56
David Causse <dcausse@wikimedia.org>
2021-03-12 09:22:51 +0100

    Force skinversion 1 for daily selenium tests
    Bug: T276571
    Change-Id: I66e220e6d305810fe058cf9094d5411283d491ea

--- tests/selenium/pageobjects/random.page.js
+++ tests/selenium/pageobjects/random.page.js
@@ -3,7 +3,7 @@ const Page = require( 'wdio-mediawiki/Page' );
 
 class RandomPage extends Page {
 	open() {
-		super.openTitle( 'Special:RandomPage' );
+		super.openTitle( 'Special:RandomPage', { useskinversion: 1 } );
 	}
 }
 module.exports = new RandomPage();

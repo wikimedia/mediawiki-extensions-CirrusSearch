db05153442ba6217424ce52ee9ce38b3b030bdf3
David Causse <dcausse@wikimedia.org>
2022-04-19 18:05:49 +0200

    Add unit tests for BoostedQueriesFunction
    some cases remain commented while we support negative weights.
    Relates to T209859
    
    Change-Id: I3fcb9da1e54a288fd346b6487762f4256f940d92

---
+++ tests/phpunit/unit/Search/Rescore/BoostedQueriesFunctionTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace CirrusSearch\Search\Rescore;
+
+use CirrusSearch\CirrusTestCase;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\FunctionScore;
+use Elastica\Query\Term;
+
+/**
+ * @covers \CirrusSearch\Search\Rescore\BoostedQueriesFunction
+ */
+class BoostedQueriesFunctionTest extends CirrusTestCase {
+
+	public function provideTestData() {
+		$termQuery = new Term( [ 'field' => 'term' ] );
+		$notTermQuery = new BoolQuery();
+		$notTermQuery->addMustNot( $termQuery );
+		return [
+			'single positive query' => [
+				[ $termQuery ],
+				[ 0.3 ],
+				[
+					[
+						'weight' => 0.3,
+						'filter' => $termQuery->toArray()
+					]
+				]
+			],
+			'two positive queries' => [
+				[ $termQuery, $termQuery ],
+				[ 0.3, 0.5 ],
+				[
+					[
+						'weight' => 0.3,
+						'filter' => $termQuery->toArray()
+					],
+					[
+						'weight' => 0.5,
+						'filter' => $termQuery->toArray()
+					]
+				]
+			],
+			/*
+			 * TODO: add support for negative weights
+			'single negative query' => [
+				[ $termQuery ],
+				[ -0.3 ],
+				[
+					[
+						'weight' => 0.3,
+						'filter' => $notTermQuery->toArray()
+					]
+				]
+			],
+			'one negative query and a positive' => [
+				[ $termQuery, $termQuery ],
+				[ 0.3, -0.4 ],
+				[
+					[
+						'weight' => 0.3,
+						'filter' => $termQuery->toArray()
+					],
+					[
+						'weight' => 0.4,
+						'filter' => $notTermQuery->toArray()
+					]
+				]
+			],
+			*/
+		];
+	}
+
+	/**
+	 * @param Elastica\Query\AbstractQuery[] $queries
+	 * @param float[] $weights
+	 * @param array $expectedFunctions
+	 * @dataProvider provideTestData
+	 */
+	public function testAppend( array $queries, array $weights, array $expectedFunctions ) {
+		$boostedQuery = new BoostedQueriesFunction( $queries, $weights );
+		$functionScore = new FunctionScore();
+		$boostedQuery->append( $functionScore );
+		$functionScoreArray = $functionScore->toArray();
+		$this->assertArrayHasKey( 'function_score', $functionScoreArray );
+		$this->assertArrayHasKey( 'functions', $functionScoreArray['function_score'] );
+		$actualFunctions = $functionScoreArray['function_score']['functions'];
+		$this->assertArrayEquals( $expectedFunctions, $actualFunctions );
+	}
+}

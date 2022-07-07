1345ffc250e62a720e8576cb02a2ac3d2c8c8cc1
Mark A. Hershberger <mah@nichework.com>
2022-07-07 13:11:16 -0400

    Update signature on various count() methods
    Each produced a fatal when running `make phpunit`.  Ex:
    
        Fatal error: During inheritance of Countable: Uncaught Return type
        of CirrusSearch\Maintenance\Set::count() should either be compatible
        with Countable::count(): int, or the #[\ReturnTypeWillChange]
        attribute should be used to temporarily suppress the notice
    
    Bug: T289879
    Change-Id: I34aff7d06290064e3e855ef9f2babc6afaa93d44

--- includes/Search/InterleavedResultSet.php
+++ includes/Search/InterleavedResultSet.php
@@ -107,7 +107,7 @@ class InterleavedResultSet extends BaseSearchResultSet implements CirrusSearchRe
 	 * The return value is cast to an integer.
 	 * @since 5.1.0
 	 */
-	public function count() {
+	public function count(): int {
 		return count( $this->results );
 	}
 

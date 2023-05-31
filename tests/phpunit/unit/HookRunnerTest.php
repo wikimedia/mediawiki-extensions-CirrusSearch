52c00d1496c152bb0d563253e45441560d0a780a
Umherirrender <umherirrender_de.wp@web.de>
2023-06-01 00:24:01 +0200

    tests: Add basic test for HookRunner
    Add a basic unit test for HookRunner to improve coverage and
    validate arguments between hook runner and hook interfaces.
    
    Change-Id: Ia7c4beaa6058c07752f79216c633f6c8a31b0689

---
+++ tests/phpunit/unit/HookRunnerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CirrusSearch\Tests\Unit;
+
+use CirrusSearch\CirrusSearchHookRunner;
+use MediaWiki\Tests\HookContainer\HookRunnerTestBase;
+
+/**
+ * @covers \CirrusSearch\CirrusSearchHookRunner
+ */
+class HookRunnerTest extends HookRunnerTestBase {
+
+	public static function provideHookRunners() {
+		yield CirrusSearchHookRunner::class => [ CirrusSearchHookRunner::class ];
+	}
+}

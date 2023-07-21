0b1b8c4253b3a0bcb8f45226714a746f5982e13d
David Causse <dcausse@wikimedia.org>
2023-07-21 11:17:52 +0200

    Add ExtensionRegistryProfileRepository
    Change-Id: I3a6b6d418aecda73404e94cd235bc3adaf50b68f

---
+++ includes/Profile/ExtensionRegistryProfileRepository.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CirrusSearch\Profile;
+
+use ExtensionRegistry;
+
+class ExtensionRegistryProfileRepository implements SearchProfileRepository {
+	private string $type;
+	private string $name;
+	private string $attributeName;
+	private ExtensionRegistry $extensionRegistry;
+
+	/**
+	 * @param string $type
+	 * @param string $name
+	 * @param string $attributeName
+	 * @param ExtensionRegistry $extensionRegistry
+	 */
+	public function __construct( string $type, string $name, string $attributeName, ExtensionRegistry $extensionRegistry ) {
+		$this->type = $type;
+		$this->name = $name;
+		$this->attributeName = $attributeName;
+		$this->extensionRegistry = $extensionRegistry;
+	}
+
+	public function repositoryType() {
+		return $this->type;
+	}
+
+	public function repositoryName() {
+		return $this->name;
+	}
+
+	public function getProfile( $name ) {
+		$profiles = $this->extractAttribute();
+		return $profiles[$name] ?? null;
+	}
+
+	public function extractAttribute(): array {
+		$profiles = $this->extensionRegistry->getAttribute( $this->attributeName );
+		if ( !is_array( $profiles ) ) {
+			throw new SearchProfileException( "Attribute {configEntry} must be an array or unset" );
+		}
+		return $profiles;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function hasProfile( $name ) {
+		return isset( $this->extractAttribute()[$name] );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function listExposedProfiles() {
+		return $this->extractAttribute();
+	}
+}
---
+++ tests/phpunit/unit/Profile/ExtensionRegistryProfileRepositoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CirrusSearch\Profile;
+
+use CirrusSearch\CirrusTestCase;
+
+/**
+ * @covers \CirrusSearch\Profile\ExtensionRegistryProfileRepository
+ */
+class ExtensionRegistryProfileRepositoryTest extends CirrusTestCase {
+
+	public function test() {
+		$registry = new \ExtensionRegistry();
+		$profiles = [
+			'prof1' => [],
+			'prof2' => [],
+		];
+		$scope = $registry->setAttributeForTest( 'my_attribute', $profiles );
+		$repo = new ExtensionRegistryProfileRepository( 'my_type', 'my_name', 'my_attribute', $registry );
+		$this->assertEquals( 'my_type', $repo->repositoryType() );
+		$this->assertEquals( 'my_name', $repo->repositoryName() );
+		$this->assertTrue( $repo->hasProfile( 'prof1' ) );
+		$this->assertTrue( $repo->hasProfile( 'prof2' ) );
+		$this->assertFalse( $repo->hasProfile( 'prof3' ) );
+		$this->assertSame( $repo->getProfile( 'prof1' ), $profiles['prof1'] );
+		$this->assertSame( $repo->listExposedProfiles(), $profiles );
+	}
+}

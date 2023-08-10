d3115ebe105b3c71d6d222d455bd30bf99b750d6
Umherirrender <umherirrender_de.wp@web.de>
2023-08-10 23:38:12 +0200

    build: Move eslint exclude folders from Gruntfile.js into .eslintrc.json
    Avoid two places, which results also in unhelpful messages:
    tests\phpunit\fixtures\configDump\enwiki_crosslang_frwiki_config.json
       0:0  warning  File ignored because of a matching ignore pattern. Use
    "--no-ignore" to override
    
    tests\phpunit\fixtures\configDump\enwiki_crosslang_frwiki_invalid_config.json
       0:0  warning  File ignored because of a matching ignore pattern. Use
    "--no-ignore" to override
    
    tests\phpunit\fixtures\configDump\enwiki_sisterproject_configs.json
       0:0  warning  File ignored because of a matching ignore pattern. Use
    "--no-ignore" to override
    
    tests\phpunit\fixtures\regexParser\ref_impl_fixtures.json
       0:0  warning  File ignored because of a matching ignore pattern. Use
    "--no-ignore" to override
    
    Change-Id: Iad25a3174797f785af12020c2c2a387f221c5ffb

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,10 +19,7 @@ module.exports = function ( grunt ) {
 				cache: true,
 				fix: grunt.option( 'fix' )
 			},
-			all: [
-				'**/*.{js,json}',
-				'!{vendor,node_modules}/**'
-			]
+			all: '.'
 		},
 		banana: {
 			all: [

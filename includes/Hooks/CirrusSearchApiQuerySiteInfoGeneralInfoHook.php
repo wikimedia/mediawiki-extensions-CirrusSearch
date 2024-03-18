6d0c2f8fa8e8b5b1567d13c0205567dc52fc79b7
Erik Bernhardson <ebernhardson@wikimedia.org>
2024-03-18 13:02:03 -0700

    Provide max page id in api query site info general info
    As part of externalizing the saneitizer, it needs to be able to know
    what page id it should be scanning up to. Expose that info as part
    of the general info exported by siteinfo.
    
    Bug: T358599
    Change-Id: I1a07412cf0cc408f15e6510b4e55a23a732bbfec

---
+++ includes/Hooks/CirrusSearchApiQuerySiteInfoGeneralInfoHook.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CirrusSearch\Hooks;
+
+use MediaWiki\Api\Hook\APIQuerySiteInfoGeneralInfoHook;
+use Wikimedia\Rdbms\IConnectionProvider;
+
+class CirrusSearchApiQuerySiteInfoGeneralInfoHook implements APIQuerySiteInfoGeneralInfoHook {
+
+	private $dbProvider;
+
+	public function __construct( IConnectionProvider $dbProvider ) {
+		$this->dbProvider = $dbProvider;
+	}
+
+	public function onAPIQuerySiteInfoGeneralInfo( $module, &$result ) {
+		$dbr = $this->dbProvider->getReplicaDatabase( false, 'api' );
+		$result['max-page-id'] = (int)$dbr->selectField( 'page', 'MAX(page_id)' );
+	}
+}
---
+++ tests/phpunit/integration/Hooks/CirrusSearchApiQuerySiteInfoGeneralInfoHookTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CirrusSearch\Test\Integration\Hooks;
+
+use CirrusSearch\CirrusIntegrationTestCase;
+use CirrusSearch\Hooks\CirrusSearchApiQuerySiteInfoGeneralInfoHook;
+use MediaWiki\MediaWikiServices;
+
+/**
+ * Using Database here because, in part, what we are testing is that this is a
+ * valid way to query max page id.
+ *
+ * @group Database
+ * @covers CirrusSearch\Hooks\CirrusSearchApiQuerySiteInfoGeneralInfoHook
+ */
+class CirrusSearchApiQuerySiteInfoGeneralInfoHookTest extends CirrusIntegrationTestCase {
+	public function testHappyPath() {
+		$dbProvider = MediaWikiServices::getInstance()->getConnectionProvider();
+		$hook = new CirrusSearchApiQuerySiteInfoGeneralInfoHook( $dbProvider );
+		$result = [];
+		$hook->onAPIQuerySiteInfoGeneralInfo( null, $result );
+		// It's an empty db, so no pages exist. But returning a number is good.
+		$this->assertCount( 1, $result, 1 );
+		$this->assertArrayHasKey( 'max-page-id', $result );
+		$this->assertIsInt( $result['max-page-id'] );
+		$this->assertGreaterThanOrEqual( 0, $result['max-page-id'] );
+	}
+}

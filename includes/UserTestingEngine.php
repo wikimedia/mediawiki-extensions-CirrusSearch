b8107a5233fe06026c2317bc47f8b8d7e42cae1c
Umherirrender <umherirrender_de.wp@web.de>
2024-06-07 21:05:48 +0200

    UserTestingEngine: Avoid array_key_exists on $GLOBALS in PHP 8.1+
    Passing $GLOBALS as function argument to array_key_exists creates a read
    only copy on each call
    
    Follow-Up: I9b46e7bc25365d01dbc86a9b2288559cb9e2cb8d
    Change-Id: Iabddf6d827748c502c8b7f9f24967ee8f74bf78d

--- includes/UserTestingEngine.php
+++ includes/UserTestingEngine.php
@@ -113,7 +113,11 @@ class UserTestingEngine {
 			$testConfig['buckets'][$status->getBucket()]['globals'] ?? [] );
 
 		foreach ( $globals as $key => $value ) {
-			if ( array_key_exists( $key, $GLOBALS ) ) {
+			// (T317951) Don't call array_key_exists unless we have to, as it's slow
+			// on PHP 8.1+ for $GLOBALS. When the key is set but is explicitly set
+			// to null, we still need to fall back to array_key_exists, but that's
+			// rarer.
+			if ( isset( $GLOBALS[$key] ) || array_key_exists( $key, $GLOBALS ) ) {
 				$GLOBALS[$key] = $value;
 			}
 		}

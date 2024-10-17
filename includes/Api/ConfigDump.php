a9248ad1386db396e1520fe2056710c9ed3ad08c
Erik Bernhardson <ebernhardson@wikimedia.org>
2024-10-17 08:41:21 -0700

    Exclude connections from cirrus-config-dump
    OpenSearch uses authentication by default, and our config dumping
    api exposes that publicly. This would be a similar concern for
    elasticsearch+xpack. We couldn't think of a strong use case for
    having this connection info generally available, so strip it from
    the config dump output. If we find a use case the username /
    password could be stripped, but it's questionable how useful the
    connection info is without it.
    
    Bug: T372769
    Change-Id: Ia087612b8cd30b47127f2e14b8fcb1e0ff22111f

--- includes/Api/ConfigDump.php
+++ includes/Api/ConfigDump.php
@@ -35,7 +35,6 @@ class ConfigDump extends ApiBase {
 	/** @var string[] */
 	public static $PUBLICLY_SHAREABLE_CONFIG_VARS = [
 		'CirrusSearchDisableUpdate',
-		'CirrusSearchServers',
 		'CirrusSearchConnectionAttempts',
 		'CirrusSearchSlowSearch',
 		'CirrusSearchUseExperimentalHighlighter',
@@ -105,7 +104,6 @@ class ConfigDump extends ApiBase {
 		'CirrusSearchFiletypeAliases',
 		'CirrusSearchDefaultCluster',
 		'CirrusSearchClientSideConnectTimeout',
-		'CirrusSearchClusters',
 		'CirrusSearchReplicaGroup',
 		'CirrusSearchExtraBackendLatency',
 		'CirrusSearchAllowLeadingWildcard',

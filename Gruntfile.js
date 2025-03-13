fbd8de9ab7639de6b7ea3020785d7f25dbef4e71
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-03-13 02:08:23 +0000

    build: Updating npm dependencies
    * @babel/helpers: 7.21.0 → 7.26.10
      * https://github.com/advisories/GHSA-968p-4wvh-cqc8
    * @babel/runtime: 7.21.0 → 7.26.10
      * https://github.com/advisories/GHSA-968p-4wvh-cqc8
    
    Additional changes:
    * Enable stylelint caching.
    
    Change-Id: Ie5ed16e8ecc8351e4dfbeda9d948e3094985b87c

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,9 @@ module.exports = function ( grunt ) {
 		},
 		banana: conf.MessagesDirs,
 		stylelint: {
+			options: {
+				cache: true
+			},
 			all: [
 				'**/*.{css,less}',
 				'!node_modules/**',

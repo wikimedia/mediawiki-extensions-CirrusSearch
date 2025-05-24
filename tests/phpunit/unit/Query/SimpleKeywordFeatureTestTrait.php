60afb0943deb2aff145f84d14dcc054389941d8b
Umherirrender <umherirrender_de.wp@web.de>
2025-05-24 22:53:53 +0200

    tests: Remove use of phpunit internal function TestCase::__construct
    Use @before hook (similiar to TestCase::setUp) instead
    
    Change-Id: Ia017050f1d3061694dce48cd4fadb118b9b08a1f

--- tests/phpunit/unit/Query/SimpleKeywordFeatureTestTrait.php
+++ tests/phpunit/unit/Query/SimpleKeywordFeatureTestTrait.php
@@ -18,13 +18,8 @@ trait SimpleKeywordFeatureTestTrait {
 	 */
 	private $kwAssertions;
 
-	/**
-	 * @param string|null $name
-	 * @param array $data
-	 * @param string $dataName
-	 */
-	public function __construct( $name = null, array $data = [], $dataName = '' ) {
-		parent::__construct( $name, $data, $dataName );
+	/** @before */
+	protected function simpleKeywordFeatureSetUp(): void {
 		$this->kwAssertions = new KeywordFeatureAssertions( $this );
 	}
 

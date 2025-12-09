82fd39594ee76667ba1d5b3736e62d84ee2c6382
James D. Forrester <jforrester@wikimedia.org>
2025-12-09 12:14:53 -0500

    Update copy of GPL license and licensing headers in files
    Bug: T400642
    Change-Id: I5256beef1b7118e8d6df99efbc6bc55b89168b4b

--- maintenance/CirrusNeedsToBeBuilt.php
+++ maintenance/CirrusNeedsToBeBuilt.php
@@ -8,20 +8,7 @@ use CirrusSearch\SearchConfig;
  * Returns zero status if a Cirrus index needs to be built for this wiki.  If
  * Elasticsearch doesn't look to be up it'll wait a minute for it to come up.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * @license GPL-2.0-or-later
  */
 
 // @codeCoverageIgnoreStart

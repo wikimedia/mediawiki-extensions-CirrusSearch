34a24bc98f3c3ae5376f72b303c4e1532a2099be
Erik Bernhardson <ebernhardson@wikimedia.org>
2026-03-24 12:06:09 -0700

    integration: Repair ability to take failure screenshots
    At some point afterTest became afterScenario in the cucumber
    context. Update the hook so it gets executed, and update it
    to match current expectations. Notably `this` is no longer
    the config object so screenshot path was hardcoded.
    
    Change-Id: I5b0107e0b28ac601f1cc75bf6cb0205ef0ecd102

--- tests/integration/config/wdio.conf.js
+++ tests/integration/config/wdio.conf.js
@@ -160,9 +160,6 @@ exports.config = {
 	// Enables colors for log output.
 	coloredLogs: true,
 	//
-	// Saves a screenshot to a given path if a command fails.
-	screenshotPath: './log/',
-	//
 	// Default timeout for all waitFor* commands.
 	waitforTimeout: 20000,
 	//
@@ -275,20 +272,22 @@ exports.config = {
 	// afterCommand: function (commandName, args, result, error) {
 	// },
 	//
-	// Function to be executed after a test (in Mocha/Jasmine) or a step (in Cucumber) starts.
-	// from https://github.com/webdriverio/webdriverio/issues/269#issuecomment-306342170
-	afterTest: async function ( test ) {
+	// Function to be executed after cucumber scenario completes.
+	afterScenario: async function ( world, result ) {
 		// if test passed, ignore, else take and save screenshot
-		if ( test.passed ) {
+		if ( result.passed ) {
 			return;
 		}
 		// get current test title and clean it, to use it as file name
-		const filename = encodeURIComponent( test.title.replace( /\s+/g, '-' ) );
+		const filename = encodeURIComponent( world.pickle.name.replace( /\s+/g, '-' ) );
 		// build file path
-		const filePath = this.screenshotPath + filename + '.png';
-		// save screenshot
-		await browser.saveScreenshot( filePath );
-		console.log( '\n\tScreenshot location:', filePath, '\n' );
+		const filePath = relPath( './integration/log/' + filename + '.png' );
+		try {
+			await browser.saveScreenshot( filePath );
+			console.log( '\n\tScreenshot location:', filePath, '\n' );
+		} catch ( screenshotError ) {
+			console.error( '\n\tFailed to take screenshot:', screenshotError.message, '\n' );
+		}
 	},
 	//
 	// Hook that gets executed after the suite has ended

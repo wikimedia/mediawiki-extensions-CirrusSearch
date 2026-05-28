c3f7981553efb6c0c7f97b5792573d163272bc38
David Causse <dcausse@wikimedia.org>
2026-05-28 15:27:09 +0200

    A version test to ConfigUtilsTest
    Bug: T427070
    Change-Id: Id9667f0d7e9f01917a1d1e1cdf437b9b50cdc0e7

--- tests/phpunit/unit/Maintenance/ConfigUtilsTest.php
+++ tests/phpunit/unit/Maintenance/ConfigUtilsTest.php
@@ -4,6 +4,7 @@ namespace CirrusSearch\Maintenance;
 
 use CirrusSearch\CirrusTestCase;
 use CirrusSearch\Test\NoopPrinter;
+use Elastica\Response;
 
 class ConfigUtilsTest extends CirrusTestCase {
 	public static function scanAvailablePluginsProvider() {
@@ -54,5 +55,66 @@ class ConfigUtilsTest extends CirrusTestCase {
 		$availablePlugins = $utils->scanAvailablePlugins( $bannedPlugins );
 		$this->assertStatusGood( $availablePlugins );
 		$this->assertEquals( $expectedPlugins, $availablePlugins->getValue() );
+	}
+
+	public function provideTestCheckElasticVersion(): \Generator {
+		yield 'opensearch 1.3.20' => [
+			true,
+			[
+				"version" => [
+					"distribution" => "opensearch",
+					"number" => "1.3.20"
+				]
+			]
+		];
+		yield 'opensearch 2.19.5' => [
+			false,
+			[
+				"version" => [
+					"distribution" => "opensearch",
+					"number" => "2.19.5"
+				]
+			]
+		];
+		yield 'opensearch 3.6.0' => [
+			false,
+			[
+				"version" => [
+					"distribution" => "opensearch",
+					"number" => "3.6.0"
+				]
+			]
+		];
+		yield 'elasticsearch 7.10.2' => [
+			true,
+			[
+				"version" => [
+					"number" => "7.10.2"
+				]
+			]
+		];
+		yield 'elasticsearch 6.8.23' => [
+			false,
+			[
+				"version" => [
+					"number" => "6.8.23"
+				]
+			]
+		];
+	}
+
+	/**
+	 * @param bool $supported
+	 * @param array $response
+	 * @return void
+	 * @covers \CirrusSearch\Maintenance\ConfigUtils::checkElasticsearchVersion
+	 * @dataProvider provideTestCheckElasticVersion
+	 */
+	public function testCheckElasticVersion( bool $supported, array $response ): void {
+		$client = $this->createMock( \Elastica\Client::class );
+		$client->expects( $this->once() )->method( 'request' )->with( '' )->willReturn( new Response( $response, 200 ) );
+		$configUtil = new ConfigUtils( $client, new NoopPrinter() );
+		$status = $configUtil->checkElasticsearchVersion();
+		$this->assertEquals( $supported, $status->isGood() );
 	}
 }
